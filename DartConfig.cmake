259f9dff8261db81b33162ada8d3904c8487dfae
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-01-14 14:57:22 +0000

    *** empty log message ***
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@286 534d5172-be3c-0410-9a3e-ccaa6372a113

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -3,4 +3,7 @@ SET (DROP_SITE "http://opium.acfr.usyd.edu.au:8081")
 SET (DROP_LOCATION "orca2")
 SET (COMPRESS_SUBMISSION ON)
 
-SET (NIGHTLY_START_TIME "21:00:00 EDT")
+# Dashboard is opened for submissions for a 24 hour period starting at
+# the specified NIGHLY_START_TIME. Time is specified in 24 hour format.
+# Sydney/Australia time zone = EST
+SET (NIGHTLY_START_TIME "23:00:00 EST")
