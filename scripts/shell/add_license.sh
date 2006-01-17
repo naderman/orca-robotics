052f3551fc512edaf29b54e219821c13401d434d
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-01-17 07:02:27 +0000

    Changed licenses
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@307 534d5172-be3c-0410-9a3e-ccaa6372a113

--- scripts/shell/add_license.sh
+++ scripts/shell/add_license.sh
@@ -1,16 +1,55 @@
 #!/bin/sh
 
-search_string=" *  You should have received a copy of the GNU"
+CHANGE_LICENSES=yes
+FOR_REAL=yes
 
-for file in `find .. -name *.h` `find .. -name *.cpp` `find .. -name *.c`; do
+toplevel=../..
+search_string=" *  You should have received a copy of the GNU"
+gpl_signature="GNU General"
+
+for file in `find $toplevel -name *.h`   \
+            `find $toplevel -name *.cpp` \
+            `find $toplevel -name *.c`   \
+            `find $toplevel -name *.ice`
+do
 
     if `grep -q "$search_string" $file`; then
-        true
+
+        # Has a license 
+        if [ "$CHANGE_LICENSES" == "yes" ]; then
+
+            # check if it's GPL
+            if `grep -q "$gpl_signature" $file`; then
+                license_end_line=`grep --line-number --max-count=1 "*/" $file | sed s/:.*//`
+                # echo "$file contains the GPL (ends on line $license_end_line)"
+                
+                num_lines=`wc --lines $file | sed s/\ .*//`
+                # echo " -- numLines: $num_lines"
+                cat license.txt > temp
+                cat $file | tail --lines=`expr $num_lines - $license_end_line` >> temp
+                num_licenses=`grep -c "$search_string" temp`
+                # echo " -- num_licenses: $num_licenses"
+                if [ $num_licenses -gt 1 ]; then
+                    echo "Oops: $file ended up with multiple licenses..."
+                    #exit 1
+                fi
+
+                echo "Replacing license in $file"
+                if [ $FOR_REAL == "yes" ]; then
+                    cat temp > $file
+                fi
+            fi
+
+        fi
+
     else
-        echo "Adding license to $file"
-        cat license.txt > temp
-        cat $file >> temp
-        cat temp > $file
+        # No license found -- add one.
+         echo "Adding license to $file"
+         cat license.txt > temp
+         cat $file >> temp
+         if [ $FOR_REAL == "yes" ]; then
+             cat temp > $file
+         fi
     fi
 
 done
