e4a6ce432b36ef67d1a36f866679d7d18b4efb7e
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-03-24 00:20:29 +0000

    docco
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@574 534d5172-be3c-0410-9a3e-ccaa6372a113

--- src/components/featuremaploader/featuremap2dI.h
+++ src/components/featuremaploader/featuremap2dI.h
@@ -10,9 +10,9 @@
 // include provided interfaces
 #include <orca/featuremap2d.h>
 
-//!
-//! Implements the Laser interface: Handles all our remote calls.
-//!
+//
+// Implements the Laser interface: Handles all our remote calls.
+//
 class FeatureMap2dI : public orca::FeatureMap2d
 {
 public:
@@ -21,14 +21,14 @@ public:
 
     // Remote calls:
 
-    //! Hand out data to people
+    // Hand out data to people
     orca::FeatureMap2dDataPtr getData(const Ice::Current&) const;
 
-    //! We ignore this request
+    // We ignore this request
     virtual void subscribe(const ::orca::FeatureMap2dConsumerPrx&,
                            const Ice::Current&);
 
-    //! We ignore this request
+    // We ignore this request
     virtual void unsubscribe(const ::orca::FeatureMap2dConsumerPrx&,
                              const Ice::Current&);
 
