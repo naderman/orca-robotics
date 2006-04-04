5da353ebe9087f39a65d9b5836b1c27448d43e82
upcroft <upcroft@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-04-04 23:34:27 +0000

    *** empty log message ***
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@630 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ src/components/imageserver/conversions.cpp
@@ -0,0 +1,57 @@
+/*
+ *  Orca Project: Components for robotics.
+ *
+ *  Copyright (C) 2004-2006
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "conversions.h"
+
+namespace imageserver {
+
+using namespace std;
+
+orca::ImageFormat orcaImageMode( int mode )
+{
+    switch( mode ) 
+    {
+    case MODE_320x240_YUV422:
+    case MODE_640x480_YUV422:
+    case MODE_800x600_YUV422:
+    case MODE_1024x768_YUV422:
+    case MODE_1280x960_YUV422:
+    case MODE_1600x1200_YUV422:
+        return orca::MODEYUV422;
+    case MODE_640x480_RGB:
+    case MODE_800x600_RGB:
+    case MODE_1024x768_RGB:
+    case MODE_1280x960_RGB:
+    case MODE_1600x1200_RGB:
+        return orca::MODERGB;
+    case MODE_640x480_MONO:
+    case MODE_800x600_MONO:
+    case MODE_1024x768_MONO:
+    case MODE_1280x960_MONO:
+    case MODE_1600x1200_MONO:
+        return orca::MODEGRAY;
+    default:
+        cout << "WARNING(conversions.cpp): Unknown colour mode." << endl;
+        return orca::MODENFI;
+    }
+
+}
+
+}
---
+++ src/components/imageserver/conversions.h
@@ -0,0 +1,34 @@
+/*
+ *  Orca Project: Components for robotics.
+ *
+ *  Copyright (C) 2004-2006
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef ORCA2_IMAGE_SERVER_CONVERSIONS_H
+#define ORCA2_IMAGE_SERVER_CONVERSIONS_H
+
+#include <orca/camera.h>
+#include <libdc1394/dc1394_control.h>
+
+namespace imageserver {
+
+    // convert from dc_1394 colour mode to orca colour mode 
+    orca::ImageFormat orcaImageMode( int mode );
+
+}
+
+#endif
