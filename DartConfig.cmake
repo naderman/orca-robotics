27b045d1f6d556fc207877e7a5b021b62080fa59
tobasco <tobasco@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-04-11 06:29:58 +0000

    changed nightly build time
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@667 534d5172-be3c-0410-9a3e-ccaa6372a113

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -5,4 +5,4 @@ SET (COMPRESS_SUBMISSION ON)
 
 # Dashboard is opened for submissions for a 24 hour period starting at
 # the specified NIGHLY_START_TIME. Time is specified in 24 hour format.
-SET (NIGHTLY_START_TIME "21:00:00 EAST")
+SET (NIGHTLY_START_TIME "04:00:00 EAST")
