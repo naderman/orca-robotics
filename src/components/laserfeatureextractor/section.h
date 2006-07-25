7f593fd90312f69237023720f5537d17f7dfc092
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-07-25 08:16:20 +0000

    added extra names to copyright for libOrcaOgMap, ImageServer, ImageViewer, LaserFeatureExtractor.
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@914 534d5172-be3c-0410-9a3e-ccaa6372a113

--- src/components/laserfeatureextractor/section.h
+++ src/components/laserfeatureextractor/section.h
@@ -1,7 +1,8 @@
 /*
  * Orca Project: Components for robotics 
  *               http://orca-robotics.sf.net/
- * Copyright (c) 2004-2006 Alex Brooks, Alexei Makarenko, Tobias Kaupp
+ * Copyright (c) 2004-2006 Alex Brooks, Alexei Makarenko, Tobias Kaupp,
+ *               George Mathews, Stef Williams.
  *
  * This copy of Orca is licensed to you under the terms described in the
  * ORCA_LICENSE file included in this distribution.
