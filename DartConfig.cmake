d3e581fd879d2b7cd87715a9f0ce8f550a59cc26
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-08-13 03:02:17 +0000

    *** empty log message ***
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@1020 534d5172-be3c-0410-9a3e-ccaa6372a113

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -6,3 +6,9 @@ SET (COMPRESS_SUBMISSION ON)
 # Dashboard is opened for submissions for a 24 hour period starting at
 # the specified NIGHLY_START_TIME. Time is specified in 24 hour format.
 SET (NIGHTLY_START_TIME "04:00:00 EAST")
+FIND_PROGRAM(MEMORYCHECK_COMMAND
+    NAMES valgrind
+    PATHS
+    DOC "Path to valgrind, used for memory error detection."
+    )
+  SET(MEMORYCHECK_SUPPRESSIONS_FILE "" CACHE FILEPATH "File that contains suppressions for the memmory checker")
