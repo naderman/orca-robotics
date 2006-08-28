7430e9161dcd63db88bc84cd2246c9ef0e94b75c
tobasco <tobasco@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-08-28 02:04:36 +0000

    updated script
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@1111 534d5172-be3c-0410-9a3e-ccaa6372a113

--- scripts/dart/orca2-nightly.sh
+++ scripts/dart/orca2-nightly.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 export CVS_RSH=ssh
-export ICE_HOME=/opt/Ice-3.0.1
 export LD_LIBRARY_PATH=$ICE_HOME/lib:$LD_LIBRARY_PATH
-export ORCA2_INSTALL=/opt/orca2-nightly
 
 dashboard=$HOME/ctests/orca/orca2-nightly
 
@@ -12,11 +10,10 @@ logfile=$dashboard/gcc40.log
 echo ---------------------------------------- >> $logfile
 date >> $logfile
 echo ---------------------------------------- >> $logfile
-# alexm: isn't it done as part of ctest'ing?
-#rm -rf $dashboard/build-gcc40/* >> $logfile 2>&1
 # build and test
 /usr/bin/ctest -S $dashboard/orca2-nightly-linux-gcc40.cmake -V >> $logfile 2>&1
-# after testing, install so other nightly tests will work
+
+# install so other nightly tests will work
 cd $dashboard/build-gcc40 >> $logfile 2>&1
 sudo make install >> $logfile 2>&1
 
@@ -25,6 +22,5 @@ logfile=$dashboard/gcc41.log
 echo ---------------------------------------- >> $logfile
 date >> $logfile
 echo ---------------------------------------- >> $logfile
-#rm -rf $dashboard/build-gcc41/* >> $logfile 2>&1
 # build and test
 /usr/bin/ctest -S $dashboard/orca2-nightly-linux-gcc41.cmake -V >> $logfile 2>&1
