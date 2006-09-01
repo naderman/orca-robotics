f6ce8ce9a98f9a12a2875d1268d4a819d9dd0cd1
upcroft <upcroft@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-09-01 02:05:57 +0000

    fixed variables - had them mixed for position3d data
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@1120 534d5172-be3c-0410-9a3e-ccaa6372a113

--- scripts/matlab/logutils/parseposition2ddata.m
+++ scripts/matlab/logutils/parseposition2ddata.m
@@ -1,11 +1,11 @@
-function [time, pose] = parseposition2ddata( file )
+function [time, position2d] = parseposition2ddata( file )
 % parse file created by orca logger (ASCII)
 % output args
 %    time = each row is a separate individual timestamp
-%    pose = each row is a pose for each timestamp
+%    posistion2d = each row is a pose for each timestamp
 
-[day, month, year, hour, min, sec, posX, posY, posZ, thetaX, thetaY, thetaZ]...
+[day, month, year, hour, min, sec, posX, posY, orientation, velX, velY, velOrientation]...
      =textread(file, '%2d%*1s%2d%*1s%2d %2d%*1s%2d%*1s%f \n %f %f %f %f %f %f', 'headerlines',1);
 
 time = [day month year hour min sec];
-pose = [posX posY posZ thetaX thetaY thetaZ];
+position2d = [posX posY orientation velX velY velOrientation];
