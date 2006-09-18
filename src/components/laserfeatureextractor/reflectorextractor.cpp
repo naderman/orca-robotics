852bccb627ce3ec483233c14aedfcbccc9d0af91
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-09-18 01:35:46 +0000

    comments only
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@1146 534d5172-be3c-0410-9a3e-ccaa6372a113

--- src/components/laserfeatureextractor/reflectorextractor.cpp
+++ src/components/laserfeatureextractor/reflectorextractor.cpp
@@ -22,6 +22,8 @@ void
 ReflectorExtractor::addFeatures( const orca::LaserDataPtr    &laserData,
                                  orca::PolarFeature2dDataPtr &features )
 {
+//     cout<<"TRACE(reflectorextractor.cpp): addFeatures()" << endl;
+
     assert( laserMaxRange_ > 0.0 );
 
     const int numReturnsToGetOverSketch = 4;
@@ -35,10 +37,16 @@ ReflectorExtractor::addFeatures( const orca::LaserDataPtr    &laserData,
     double maxDeltaRangeWithinReflector = 0.0;
     int    lastSketchReturn             = -numReturnsToGetOverSketch;
 
+//     cout<<"TRACE(reflectorextractor.cpp): laserData->ranges.size(): " << laserData->ranges.size() << endl;
+//     cout<<"TRACE(reflectorextractor.cpp): laserMaxRange_: " << laserMaxRange_ << endl;
+//     cout<<"TRACE(reflectorextractor.cpp): minReflectorBrightness: " << minReflectorBrightness_ << endl;
+
     // Don't iterate over the endpoints, so that the helper-functions
     // don't have to check.
     for ( unsigned int i=1; i<laserData->ranges.size()-1; i++ )
     {
+        // cout<<"TRACE(reflectorextractor.cpp): r,i: " << laserData->ranges[i] << "," << (int)(laserData->intensities[i]) << endl;
+
         // Is this a reflector inside range?
         if ( ( laserData->intensities[i] >= minReflectorBrightness_ ) &&
              ( laserData->ranges[i] < laserMaxRange_ ) )
