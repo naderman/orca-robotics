44fb3c2cfeaf93323b9c112a7dc2b88832945325
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-11-21 06:24:59 +0000

    *** empty log message ***
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@1335 534d5172-be3c-0410-9a3e-ccaa6372a113

--- src/components/segwayrmp/usb/test/plugtest.cpp
+++ src/components/segwayrmp/usb/test/plugtest.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 #define SEGWAY_USB_VENDOR_ID                    0x0403
 #define SEGWAY_USB_PRODUCT_ID                   0xE729
-#define SEGWAY_USB_VENDOR_PRODUCT_ID            0x0403E729
+#define SEGWAY_USB_DESCRIPTION                  "Robotic Mobile Platform"
 
 void open( FT_HANDLE &ftHandle_ )
 {
@@ -21,7 +21,7 @@ void open( FT_HANDLE &ftHandle_ )
     strcpy(Description,SEGWAY_USB_DESCRIPTION);
     ftStatus = FT_OpenEx(Description ,FT_OPEN_BY_DESCRIPTION, &ftHandle_ );
     if ( ftStatus != FT_OK) {
-        cout<<"FT_OpenEx failed ("<<ftStatusToString(ftStatus)<<")"<<endl;
+        cout<<"FT_OpenEx failed ("<<ftStatus<<")"<<endl;
         exit(1);
     }
     cout<<"FT_OpenEx OK"<<endl;
@@ -31,7 +31,7 @@ void close( FT_HANDLE &ftHandle_ )
 {
     FT_STATUS ftStatus = FT_Close( ftHandle_ );
     if ( ftStatus != FT_OK )
-        cout << "ERROR(usbftdi.cpp): FT_Close failed: " << ftStatusToString(ftStatus) << endl;
+        cout << "ERROR(usbftdi.cpp): FT_Close failed: " << ftStatus << endl;
     else
         cout<<"TRACE(plugtest.cpp): FT_Close worked." << endl;
 }
@@ -44,7 +44,7 @@ int main()
 
     cout<<"Un-plug then re-plug the USB, then hit return." << endl;
 
-    getch();
+    getchar();
 
     close( ftHandle_ );
     open( ftHandle_ );
