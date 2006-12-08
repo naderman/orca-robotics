30d760d5baa7825b98caf0fe16f121b4f4e3a253
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-12-08 05:56:40 +0000

    *** empty log message ***
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@1460 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ src/libs/orcaqcm/orcaqcm.h
@@ -0,0 +1,18 @@
+/*
+ * Orca Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2006 Alex Brooks, Alexei Makarenko, Tobias Kaupp
+ *
+ * This copy of Orca is licensed to you under the terms described in the
+ * ORCA_LICENSE file included in this distribution.
+ *
+ */
+ 
+#ifndef ORCAQCM_ORCAQCM_H
+#define ORCAQCM_ORCAQCM_H
+
+#include <orcaqcm/ocmmodel.h>
+#include <orcaqcm/ocmdelegate.h>
+#include <orcaqcm/orcaicons.h>
+ 
+#endif
