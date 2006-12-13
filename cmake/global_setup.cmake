26427cbb51cf01e07ba8e22c43b1c1d2cc7b2e87
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-12-13 11:12:46 +0000

    f-no-stack-blahblah for gcc 4.1 only
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@1510 534d5172-be3c-0410-9a3e-ccaa6372a113

--- cmake/global_setup.cmake
+++ cmake/global_setup.cmake
@@ -104,9 +104,23 @@ IF ( ${CMAKE_C_COMPILER} MATCHES gcc )
     ENDIF ( GCC_VERSION MATCHES ".*4\\.[0-9]\\.[0-9]")
 
     ASSERT ( GCC_VERSION_OK
-             "Checking gcc version - failed. Orca2 requires gcc v. 4.x"
-             "Checking gcc version - ok"
-             1 )
+      "Checking gcc version - failed. Orca2 requires gcc v. 4.x"
+      "Checking gcc version - ok"
+      1 )
+    
+    IF ( GCC_VERSION MATCHES ".*4\\.0.*" )
+      # gcc 4.0.x
+    ENDIF ( GCC_VERSION MATCHES ".*4\\.0.*" )
+    IF ( GCC_VERSION MATCHES ".*4\\.1.*" )
+      # gcc 4.1.x
+      # gcc-4.1 adds stack protection, which makes code robust to buffer-overrun attacks
+      #      (see: http://www.trl.ibm.com/projects/security/ssp/)
+      # However for some reason this can result in the symbol '__stack_chk_fail_local' not being found.
+      # So turn it off.
+      ADD_DEFINITIONS( -fno-stack-protector )
+    ENDIF ( GCC_VERSION MATCHES ".*4\\.1.*" )
+
+
 ENDIF ( ${CMAKE_C_COMPILER} MATCHES gcc )
 
 ###########################################################
