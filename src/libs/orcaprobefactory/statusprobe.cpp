45c548a0ea6ce1d386fa5f9e6bd6b4464c286d29
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-12-16 14:34:18 +0000

    oops
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@1547 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ src/libs/orcaprobefactory/statusprobe.cpp
@@ -0,0 +1,141 @@
+/*
+ * Orca Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2006 Alexei Makarenko
+ *
+ * This copy of Orca is licensed to you under the terms described in the
+ * ORCA_LICENSE file included in this distribution.
+ *
+ */
+
+#include <iostream>
+#include <orcaice/orcaice.h>
+#include <orcacm/orcacm.h>
+#include <orcaprobe/orcaprobe.h>
+
+#include "statusprobe.h"
+
+using namespace std;
+using namespace orcaprobefactory;
+
+StatusProbe::StatusProbe( const orca::FQInterfaceName & name, orcaprobe::DisplayDriver & display,
+                                const orcaice::Context & context )
+    : InterfaceProbe(name,display,context)
+{
+    id_ = "::orca::Status";
+
+    addOperation( "getData",        "nonmutating StatusData getData()" );
+    addOperation( "subscribe",      "void subscribe( StatusConsumer *subscriber )" );
+    addOperation( "unsubscribe",    "idempotent void unsubscribe( StatusConsumer *subscriber )" );
+}
+    
+int 
+StatusProbe::loadOperationEvent( const int index, orcacm::OperationData & data )
+{    
+    switch ( index )
+    {
+    case orcaprobe::UserIndex :
+        return loadGetData( data );
+    case orcaprobe::UserIndex+1 :
+        return loadSubscribe( data );
+    case orcaprobe::UserIndex+2 :
+        return loadUnsubscribe( data );
+    }
+    return 1;
+}
+
+int 
+StatusProbe::loadGetData( orcacm::OperationData & data )
+{
+    orca::StatusDataPtr result;
+    orcacm::ResultHeader res;
+    
+    try
+    {
+        orca::StatusPrx derivedPrx = orca::StatusPrx::checkedCast(prx_);
+        result = derivedPrx->getData();
+    }
+    catch( const Ice::Exception & e )
+    {
+        stringstream ss;
+        ss << e;
+        res.name = "exception";
+        res.text = ss.str();
+        data.results.push_back( res );
+        return 1;
+    }
+
+//     cout<<orcaice::toString(result)<<endl;
+    res.name = "data";
+    res.text = orcaice::toString(result);
+    data.results.push_back( res );
+    return 0;
+}
+
+int 
+StatusProbe::loadSubscribe( orcacm::OperationData & data )
+{
+    Ice::ObjectPtr consumer = this;
+    orca::StatusConsumerPrx callbackPrx = 
+            orcaice::createConsumerInterface<orca::StatusConsumerPrx>( context_, consumer );
+
+    orcacm::ResultHeader res;
+
+    try
+    {
+        orca::StatusPrx derivedPrx = orca::StatusPrx::checkedCast(prx_);
+        derivedPrx->subscribe( callbackPrx );
+    }
+    catch( const Ice::Exception & e )
+    {
+        stringstream ss;
+        ss << e;
+        res.name = "exception";
+        res.text = ss.str();
+        data.results.push_back( res );
+        return 1;
+    }
+    
+    res.name = "outcome";
+    res.text = "Subscribed successfully";
+    data.results.push_back( res );
+    return 0;
+}
+
+int 
+StatusProbe::loadUnsubscribe( orcacm::OperationData & data )
+{
+//     try
+//     {
+//         orca::PowerPrx derivedPrx = orca::PowerPrx::checkedCast(prx_);
+// //         cout<<"unsub  "<<Ice::identityToString( consumerPrx_->ice_getIdentity() )<<endl;
+//         
+//         orca::PowerConsumerPrx powerConsumerPrx = orca::PowerConsumerPrx::uncheckedCast(consumerPrx_);
+// //         cout<<"unsub  "<<Ice::identityToString( powerConsumerPrx->ice_getIdentity() )<<endl;
+//         derivedPrx->unsubscribe( powerConsumerPrx );
+//     }
+//     catch( const Ice::Exception & e )
+//     {
+//         cout<<"caught "<<e<<endl;
+//         return 1;
+//     }
+    
+    return 0;
+}
+
+void 
+StatusProbe::setData(const orca::StatusDataPtr& result, const Ice::Current&)
+{
+    std::cout << orcaice::toString(result) << std::endl;
+
+    orcacm::OperationData data;
+    // this is the result for operation "subscribe" which has user index=2;
+    fillOperationData( orcaprobe::UserIndex+2, data );
+
+    orcacm::ResultHeader res;
+    res.name = "data";
+    res.text = orcaice::toString(result);
+    data.results.push_back( res );
+
+    display_.setOperationData( data );
+};
---
+++ src/libs/orcaprobefactory/statusprobe.h
@@ -0,0 +1,43 @@
+/*
+ * Orca Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2006 Alexei Makarenko
+ *
+ * This copy of Orca is licensed to you under the terms described in the
+ * ORCA_LICENSE file included in this distribution.
+ *
+ */
+
+#ifndef ORCA2_ORCAPROBEFACTORY_STATUS_INTERFACE_PROBE_H
+#define ORCA2_ORCAPROBEFACTORY_STATUS_INTERFACE_PROBE_H
+
+#include <orcaprobe/interfaceprobe.h>
+#include <orca/status.h>
+
+namespace orcaprobefactory
+{
+
+class StatusProbe : public orca::StatusConsumer, public orcaprobe::InterfaceProbe
+{
+
+public:
+
+    StatusProbe( const orca::FQInterfaceName & name, orcaprobe::DisplayDriver & display,
+                                const orcaice::Context & context );
+
+    virtual int loadOperationEvent( const int index, orcacm::OperationData & data );
+
+    // from consumer
+    virtual void setData(const orca::StatusDataPtr& data, const Ice::Current&);
+    
+private:
+
+    int loadGetData( orcacm::OperationData & data );
+    int loadSubscribe( orcacm::OperationData & data );
+    int loadUnsubscribe( orcacm::OperationData & data );
+
+};
+
+} // namespace
+
+#endif
