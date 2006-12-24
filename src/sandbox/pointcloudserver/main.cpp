cc63ffb31e2ff96514215b2f735834e3b510967f
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2006-12-24 05:37:36 +0000

    not finished but need to get rid off old files first to reuse the file name.
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@1615 534d5172-be3c-0410-9a3e-ccaa6372a113

--- src/sandbox/pointcloudserver/main.cpp
+++ src/sandbox/pointcloudserver/main.cpp
@@ -1,37 +1,40 @@
-#include "pcserver.h"
+#include "pointcloudI.h"
 #include <Ice/Ice.h>
+
+namespace pcserver
+{
 
 class PointCloudServerApp : virtual public Ice::Application
 {
 public:
   
-  virtual int run(int argc, char** argv)
-  {
-    Ice::CommunicatorPtr ic;
-    ic = Ice::initialize(argc, argv);
-    // Note: This actually works if there is no parameter since argv[argc] is required to be NULL according to the 
-    // ISO C++ standard
-    orca::PointCloudServer *pcs = new orca::PointCloudServer(argv[1]);
-    printf("Setting up adaptor ...\n"); 
-    Ice::ObjectAdapterPtr adapter = communicator()->createObjectAdapterWithEndpoints("pointcloudserver", "default -p 10101 -z");
-    adapter->add(pcs, ic->stringToIdentity("PointCloudServer"));
-    adapter->activate();
-    printf("Waiting for requests ...\n"); 
-    communicator()->waitForShutdown();
+    virtual int run(int argc, char** argv)
+    {
+        Ice::CommunicatorPtr ic;
+        ic = Ice::initialize(argc, argv);
 
-    // Now we should clean up
-   
-   
-    
-    return EXIT_SUCCESS;
+        // Note: This actually works if there is no parameter since argv[argc] is required to be NULL according to the 
+        // ISO C++ standard
+        PointCloudI *pcs = new PointCloudI( argv[1] );
+
+        printf("Setting up adaptor ...\n"); 
+        Ice::ObjectAdapterPtr adapter = communicator()->createObjectAdapterWithEndpoints("pointcloudserver", "default -p 10101 -z");
+        adapter->add(pcs, ic->stringToIdentity("PointCloudI"));
+        adapter->activate();
+        printf("Waiting for requests ...\n"); 
+
+        communicator()->waitForShutdown();        
+        // Now we should clean up
+        
+        return EXIT_SUCCESS;
     }
 };
 
-
+} // namespace
 
 int main(int argc, char* argv[])
 {
   printf("starting up ..."); 
-  PointCloudServerApp app;
+  pcserver::PointCloudServerApp app;
   return app.main(argc, argv);
 }
