ce5719f76f8b52256471645ec918c7de748d5e54
upcroft <upcroft@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-02-10 01:32:59 +0000

    first commit
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@1992 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ src/components/insgps/localise3dI.cpp
@@ -0,0 +1,138 @@
+/*
+ * Orca Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2007 Ben Upcroft
+ *
+ * This copy of Orca is licensed to you under the terms described in the
+ * ORCA_LICENSE file included in this distribution.
+ *
+ */
+
+#include <iostream>
+#include <orcaice/orcaice.h>
+
+#include "localise3dI.h"
+
+using namespace std;
+using namespace orca;
+using namespace insgps;
+
+Localise3dI::Localise3dI( const VehicleDescription&  descr,
+                          Driver*                   hwDriver,
+                          const orcaice::Context & context )
+    :   InsGpsI(context),
+        descr_(descr),
+        hwDriver_(hwDriver),
+        context_(context)
+{
+    //
+    // EXTERNAL PROVIDED INTERFACE: Localise3d
+    //
+    // Find IceStorm Topic to which we'll publish
+    // the main topic is 'name/*@platform/component'
+    topicPrx_ = orcaice::connectToTopicWithTag<Localise3dConsumerPrx>
+            ( context_, localise3dPublisher_, "Localise3d" );
+}
+
+void
+Localise3dI::publish()
+{
+   
+    // blocking read with timeout (2000ms by default)
+    // get localise3d
+    read( localise3dData_ );
+    
+    // send the data to icestorm and to a buffer for direct connections
+    localSetData( localise3dData_ );
+            
+    return;
+}
+
+//
+// local calls
+//
+
+void
+Localise3dI::read( ::orca::Localise3dData& data )
+{
+    hwDriver_->readLocalise3d( data );
+}         
+
+
+//
+// remote calls
+//
+
+orca::Localise3dData
+Localise3dI::getData(const Ice::Current& current) const
+{
+//     std::cout << "getData()" << std::endl;
+    orca::Localise3dData data;
+    // we don't need to pop the data here because we don't block on it.
+    if ( localise3dDataBuffer_.isEmpty() )
+    {
+        cout << "ERROR(localise3dI.cpp): getData() was called when no data had been generated!!" << endl;
+        throw orca::DataNotExistException( "Localise3d proxy is not populated yet" );
+    }else{
+        localise3dDataBuffer_.get( data );
+    }
+    return data;
+}
+
+
+::orca::Localise3dData
+Localise3dI::getDataAtTime(const orca::Time& timeStamp, const ::Ice::Current& ) const
+{
+    std::string errString = "localise3dI:getDataAtTime(): NOT IMPLEMENTED YET";
+    context_.tracer()->error( errString );
+    throw orcaice::Exception( ERROR_INFO, errString );
+
+    ::orca::Localise3dData data;
+    return data;
+}
+
+::orca::VehicleDescription
+Localise3dI::getDescription(const ::Ice::Current& ) const
+{
+    std::cout << "getDescription()" << std::endl;
+    return descr_;
+}
+
+// Subscribe people
+void 
+Localise3dI::subscribe(const ::orca::Localise3dConsumerPrx &subscriber, const ::Ice::Current&)
+{
+    cout << "subscribe()" << endl;
+    IceStorm::QoS qos;
+    qos["reliability"] = "twoway";
+    topicPrx_->subscribe( qos, subscriber );
+}
+
+// Unsubscribe people
+void 
+Localise3dI::unsubscribe(const ::orca::Localise3dConsumerPrx &subscriber, const ::Ice::Current&)
+{
+    cout << "unsubscribe() Localise3d" << endl;
+    topicPrx_->unsubscribe( subscriber );
+}
+
+// Set pva IMU Data
+void
+Localise3dI::localSetData( const ::orca::Localise3dData& data )
+{
+    // Stick it in the buffer so pullers can get it
+    localise3dDataBuffer_.push( data );
+
+    try {
+        // push it to IceStorm
+        localise3dPublisher_->setData( data );
+    }
+    catch ( Ice::ConnectionRefusedException &e )
+    {
+        // This could happen if IceStorm dies.
+        // If we're running in an IceBox and the IceBox is shutting down,
+        // this is expected (our co-located IceStorm is obviously going down).
+        context_.tracer()->warning( "Failed push to IceStorm." );
+    }
+}
+
---
+++ src/components/insgps/localise3dI.h
@@ -0,0 +1,100 @@
+/*
+ * Orca Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2007 Ben Upcroft
+ *
+ * This copy of Orca is licensed to you under the terms described in the
+ * ORCA_LICENSE file included in this distribution.
+ *
+ */
+
+#ifndef ORCA2_INSGPS_LOCALISE3DI_H
+#define ORCA2_INSGPS_LOCALISE3DI_H
+
+#include <IceStorm/IceStorm.h>
+
+// include provided interfaces
+#include <orca/localise3d.h>
+
+// for context()
+#include <orcaice/orcaice.h>
+
+// utilities
+#include <orcaice/ptrbuffer.h>
+
+// hardware driver      
+#include "driver.h"
+
+// base class
+#include "insgpsi.h"
+
+//
+// Implements the Localise3d interface:
+//     - Reads the localise3d messages provided by the driver and publishes them
+//     - Handles all our remote calls.
+//
+// The component interacts with hardware and the outside
+// world through the handler and (thread-safe) buffers.
+//
+
+class Localise3dI : public orca::Localise3d, public insgps::InsGpsI
+{
+public:
+    Localise3dI(const orca::VehicleDescription& descr,
+                insgps::Driver*  hwDriver,
+                const orcaice::Context & context);
+
+    //
+    // localise3d message handler functions
+    //
+     
+    // the handler calls this function which reads from the hwDriver_'s  buffers
+    // and then publishes to the outside world   
+    virtual void publish();
+   
+    //
+    // remote calls:
+    //
+    
+    // Get pva localise3d Data
+    virtual ::orca::Localise3dData getData(const ::Ice::Current& ) const;
+    virtual ::orca::Localise3dData getDataAtTime(const orca::Time&, const Ice::Current&) const;
+    virtual ::orca::VehicleDescription getDescription(const ::Ice::Current& ) const;
+
+    // Subscribe and unsubscribe people
+    virtual void subscribe(const ::orca::Localise3dConsumerPrx&, const ::Ice::Current& = ::Ice::Current());
+    virtual void unsubscribe(const ::orca::Localise3dConsumerPrx&, const ::Ice::Current& = ::Ice::Current());
+
+    // Set pos3d Data
+    void localSetData( const ::orca::Localise3dData& data );
+    
+private:
+
+    // the driver will put the latest pva data into this buffer
+    orcaice::Buffer<orca::Localise3dData> localise3dDataBuffer_;
+
+    //publishers
+    orca::Localise3dConsumerPrx localise3dPublisher_;
+
+    //topics
+    IceStorm::TopicPrx topicPrx_;
+
+    orca::VehicleDescription descr_;
+
+    //
+    // handler stuff
+    //
+    
+    // hardware driver   
+    insgps::Driver* hwDriver_;
+
+    orca::Localise3dData localise3dData_;
+    
+    // read from the hwDriver_'s buffer
+    void read( ::orca::Localise3dData& );
+    
+    orcaice::Context context_;
+
+};
+
+#endif
