9751f782d6694ed9927ec085c110810e73162f83
upcroft <upcroft@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-03-12 20:19:56 +0000

    moved from components/localnav/localnavutil
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@2114 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ src/libs/orcalocalnav/velocity.cpp
@@ -0,0 +1,38 @@
+/*
+ * Orca Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2006 Alex Brooks, Alexei Makarenko, Tobias Kaupp
+ *
+ * This copy of Orca is licensed to you under the terms described in the
+ * ORCA_LICENSE file included in this distribution.
+ *
+ */
+
+#include "velocity.h"
+#include <iostream>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+namespace orcalocalnav {
+
+// std::string 
+// Velocity::toString() const
+// {
+//     stringstream ss;
+//     ss << "["<<lin()<<"m/s, "<<
+// }
+
+std::ostream &operator<<( std::ostream &s, const Velocity &v )
+{
+    return s <<"["<<v.lin()<<"m/s, "<<v.rot()*180.0/M_PI<<"deg/s]";
+}
+
+std::ostream &operator<<( std::ostream &s, const Acceleration &v )
+{
+    return s <<"["<<v.lin()<<"m/s/s, "<<v.rot()*180.0/M_PI<<"deg/s/s]";
+}
+
+
+}
