89e9a086ee4c0c233dbe7959e8d2b119ef548e4f
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-03-16 11:26:13 +0000

    docco only.
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@2155 534d5172-be3c-0410-9a3e-ccaa6372a113

--- src/utils/icegridmon/mainloop.h
+++ src/utils/icegridmon/mainloop.h
@@ -1,7 +1,7 @@
 /*
  * Orca Project: Components for robotics 
  *               http://orca-robotics.sf.net/
- * Copyright (c) 2006-2007 Alexei Makarenko
+ * Copyright (c) 2006-2007 Alexei Makarenko, Alex Brooks
  *
  * This copy of Orca is licensed to you under the terms described in the
  * ORCA_LICENSE file included in this distribution.
