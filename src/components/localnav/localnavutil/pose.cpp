e71f20a2bd3b9a058fa15d159735d0ef507b6d7a
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-03-19 06:17:43 +0000

    cleaned up.
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@2182 534d5172-be3c-0410-9a3e-ccaa6372a113

--- src/components/localnav/localnavutil/pose.cpp
+++ src/components/localnav/localnavutil/pose.cpp
@@ -9,19 +9,6 @@ using namespace std;
 
 namespace localnav {
 
-std::string
-Pose::toString() const
-{
-    stringstream ss;
-    ss << "[" << x() << ", " << y() << ", " << theta()*180.0/M_PI << "deg]";
-    return ss.str();
-}
-
-std::ostream &operator<<( std::ostream &s, const Pose &p )
-{
-    return s << p.toString();
-}
-
 orcanavutil::Pose
 getMLPose( const orca::Localise2dData &localiseData )
 {
--- src/components/localnav/localnavutil/pose.h
+++ src/components/localnav/localnavutil/pose.h
@@ -1,54 +1,18 @@
 #ifndef POSE_H
 #define POSE_H
 
-#include <orcageom2d/geom2d.h>
 #include <cmath>
-#include <iostream>
-#include <string>
-
-// for getMLPose()
+#include <orcageom2d/geom2d.h>
 #include <orcanavutil/pose.h>
 #include <orca/localise2d.h>
 
 
 namespace localnav {
 
-//!
-//! @author 
-//!
-class Pose
-{
-
-public: 
-
-    Pose() {};
-    Pose( double x, double y, double theta )
-        :x_(x),y_(y),theta_(theta) {}
-
-    // non-const versions
-    double &x()     { return x_; }
-    double &y()     { return y_; }
-    double &theta() { return theta_; }
-    
-    // const versions
-    double x()     const { return x_; }
-    double y()     const { return y_; }
-    double theta() const { return theta_; }
-
-    std::string toString() const;
-
-private: 
-
-    double x_, y_, theta_;
-
-};
-
-inline double distance( const Pose &pose, const geom2d::Point &point )
+inline double distance( const orcanavutil::Pose &pose, const geom2d::Point &point )
 { return hypotf( pose.y()-point.y(), pose.x()-point.x() ); }
-inline double distance( const geom2d::Point &point, const Pose &pose )
+inline double distance( const geom2d::Point &point, const orcanavutil::Pose &pose )
 { return distance( pose, point ); }
-
-std::ostream &operator<<( std::ostream &s, const Pose &p );
 
 orcanavutil::Pose getMLPose( const orca::Localise2dData &localiseData );
 
