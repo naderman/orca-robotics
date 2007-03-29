9437bc0f02842d54fee17ee61d7bfb28dadc8c05
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-03-29 02:27:58 +0000

    *** empty log message ***
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@2242 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ src/libs/orcaifaceimpl/cameraiface.cpp
@@ -0,0 +1,152 @@
+/*
+ * Orca Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2007 Alex Brooks, Alexei Makarenko, Tobias Kaupp
+ *
+ * This copy of Orca is licensed to you under the terms described in the
+ * ORCA_LICENSE file included in this distribution.
+ *
+ */
+
+#include <iostream>
+
+#include <orcaice/orcaice.h>
+#include "cameraiface.h"
+#include "util.h"
+
+using namespace std;
+
+namespace orcaifaceimpl {
+
+//////////////////////////////////////////////////////////////////////
+
+//
+// This is the implementation of the slice-defined interface
+//
+class CameraI : public orca::Camera
+{
+public:
+    //! constructor
+    CameraI( CameraIface &iface )
+        : iface_(iface) {}
+
+    // remote interface
+
+    virtual ::orca::CameraData getData(const ::Ice::Current& ) const
+        { return iface_.getData(); }
+
+    virtual ::orca::CameraDescription getDescription(const ::Ice::Current& ) const
+        { return iface_.getDescription(); }
+
+    virtual void subscribe(const ::orca::CameraConsumerPrx& consumer,
+                           const ::Ice::Current& = ::Ice::Current())
+        { iface_.subscribe( consumer ); }
+
+    virtual void unsubscribe(const ::orca::CameraConsumerPrx& consumer,
+                             const ::Ice::Current& = ::Ice::Current())
+        { iface_.subscribe( consumer ); }
+
+private:
+    CameraIface &iface_;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+CameraIface::CameraIface( const orca::CameraDescription& descr,
+                          const std::string& ifaceTag,
+                          const orcaice::Context& context )
+    : descr_(descr),
+      tag_(ifaceTag),
+      context_(context)
+{
+}
+
+CameraIface::~CameraIface()
+{
+    tryRemovePtr( context_, ptr_ );
+}
+
+void
+CameraIface::initInterface()
+{
+    // Find IceStorm Topic to which we'll publish
+    topicPrx_ = orcaice::connectToTopicWithTag<orca::CameraConsumerPrx>
+        ( context_, consumerPrx_, tag_ );
+
+    // Register with the adapter
+    // We don't have to clean up the memory we're allocating here, because
+    // we're holding it in a smart pointer which will clean up when it's done.
+    ptr_ = new CameraI( *this );
+    orcaice::createInterfaceWithTag( context_, ptr_, tag_ );
+}
+
+::orca::CameraData 
+CameraIface::getData() const
+{
+    context_.tracer()->debug( "CameraIface::getData()", 5 );
+
+    if ( dataProxy_.isEmpty() )
+    {
+        std::stringstream ss;
+        ss << "No data available! (ifaceTag="<<tag_<<")";
+        throw orca::DataNotExistException( ss.str() );
+    }
+
+    orca::CameraData data;
+    dataProxy_.get( data );
+    return data;
+}
+
+::orca::CameraDescription
+CameraIface::getDescription() const
+{
+    return descr_;
+}
+
+void 
+CameraIface::subscribe(const ::orca::CameraConsumerPrx& subscriber)
+{
+    context_.tracer()->debug( "CameraIface::subscribe(): subscriber='"+subscriber->ice_toString()+"'", 4 );
+    IceStorm::QoS qos;
+    qos["reliability"] = "twoway";
+    try {
+        topicPrx_->subscribe( qos, subscriber );
+    }
+    catch ( const Ice::Exception & e ) {
+        std::stringstream ss;
+        ss <<"CameraIface::subscribe: failed to subscribe: "<< e << endl;
+        context_.tracer()->warning( ss.str() );
+        throw orca::SubscriptionFailedException( ss.str() );
+    }
+}
+
+void 
+CameraIface::unsubscribe(const ::orca::CameraConsumerPrx& subscriber)
+{
+    context_.tracer()->debug( "CameraIface::unsubscribe(): subscriber='"+subscriber->ice_toString()+"'", 4 );
+    topicPrx_->unsubscribe( subscriber );
+}
+
+void
+CameraIface::localSet( const orca::CameraData& data )
+{
+    dataProxy_.set( data );
+}
+
+void
+CameraIface::localSetAndSend( const orca::CameraData& data )
+{
+//     cout<<"TRACE(CameraIface.cpp): localSetAndSend: " << orcaice::toString(data) << endl;
+
+    dataProxy_.set( data );
+
+    // Try to push to IceStorm.
+    tryPushToIceStormWithReconnect<orca::CameraConsumerPrx,orca::CameraData>
+        ( context_,
+        consumerPrx_,
+        data,
+        topicPrx_,
+        tag_ );
+}
+
+} // namespace
---
+++ src/libs/orcaifaceimpl/cameraiface.h
@@ -0,0 +1,77 @@
+/*
+ * Orca Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2007 Alex Brooks, Alexei Makarenko, Tobias Kaupp
+ *
+ * This copy of Orca is licensed to you under the terms described in the
+ * ORCA_LICENSE file included in this distribution.
+ *
+ */
+
+#ifndef ORCA2_CAMERA_IFACE_H
+#define ORCA2_CAMERA_IFACE_H
+
+#include <orca/camera.h>
+#include <IceStorm/IceStorm.h>
+
+// utilities
+#include <orcaice/proxy.h>
+#include <orcaice/context.h>
+
+namespace orcaifaceimpl {
+
+//!
+//! Implements the orca::Camera interface. Handles remote calls.
+//!
+class CameraIface : public IceUtil::Shared
+{
+public:
+    //! constructor
+    CameraIface( const orca::CameraDescription& descr,
+                 const std::string& ifaceTag, 
+                 const orcaice::Context& context );
+    ~CameraIface();
+
+    // remote interface
+
+    ::orca::CameraData getData() const;
+
+    ::orca::CameraDescription getDescription() const;
+
+    void subscribe(const ::orca::CameraConsumerPrx&);
+
+    void unsubscribe(const ::orca::CameraConsumerPrx&);
+
+
+    // local interface:
+
+    //! May throw orcaice::Exceptions.
+    void initInterface();
+
+    //! A local call which sets the data reported by the interface
+    void localSet( const orca::CameraData& data );
+
+    //! A local call which sets the data reported by the interface, 
+    //! and sends it through IceStorm
+    void localSetAndSend( const orca::CameraData& data );
+
+private:
+
+    orca::CameraDescription     descr_;
+    orcaice::Proxy<orca::CameraData> dataProxy_;
+
+    orca::CameraConsumerPrx    consumerPrx_;
+    IceStorm::TopicPrx             topicPrx_;
+
+    // Hang onto this so we can remove from the adapter and control when things get deleted
+    Ice::ObjectPtr          ptr_;
+
+    const std::string              tag_;
+    orcaice::Context               context_;
+};
+
+typedef IceUtil::Handle<CameraIface> CameraIfacePtr;
+
+}
+
+#endif
