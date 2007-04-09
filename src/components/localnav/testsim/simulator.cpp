81f3f93db776593c292591d764c7a2181c53fece
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-04-09 03:11:26 +0000

    moved unmaintained noisyrobot2d to sandbox.
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@2277 534d5172-be3c-0410-9a3e-ccaa6372a113

--- src/components/localnav/testsim/simulator.cpp
+++ src/components/localnav/testsim/simulator.cpp
@@ -27,7 +27,8 @@ Simulator::Simulator( const orcaice::Context &context,
 {
     // Set up pose
     pose_.x() = 0;
-    pose_.y() = 0;
+//    pose_.y() = 0;
+    pose_.y() = -8;
     pose_.theta() = 0;
 
     Ice::PropertiesPtr prop = context.properties();
@@ -68,7 +69,8 @@ Simulator::Simulator( const orcaice::Context &context,
 
     // setup velocity.
     // Start negative linear, to make sure the driver can hangle it.
-    velLin_ = -1.0;
+//    velLin_ = -1.0;
+    velLin_ = 0.0;
     velRot_ = 0.0;
 
     // setup interfaces
@@ -200,6 +202,7 @@ Simulator::setupMap()
 {
     ogMap_.fill( 0 );
 
+#if 0
     placeObstacle( ogMap_, 7, 4, 0.5 );
     placeObstacle( ogMap_, -2, -4, 0.5 );
     placeObstacle( ogMap_, -7, -4, 0.5 );
@@ -211,6 +214,7 @@ Simulator::setupMap()
     placeObstacle( ogMap_, 14, -18, 0.5 );
     placeObstacle( ogMap_, 16, -12, 0.5 );
     placeObstacle( ogMap_, 18, -16, 0.5 );
+#endif
 
     placeRoom( ogMap_ );
 }
