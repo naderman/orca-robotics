d45fb44ed8c75fe98d06b8f64f5f1d756aeee0b3
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-06-27 03:09:42 +0000

    *** empty log message ***
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@2635 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ src/components/laser2d/fakedriver/fakedriver.h
@@ -0,0 +1,57 @@
+/*
+ * Orca Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2007 Alex Brooks, Alexei Makarenko, Tobias Kaupp
+ *
+ * This copy of Orca is licensed to you under the terms described in the
+ * ORCA_LICENSE file included in this distribution.
+ *
+ */
+#ifndef ORCA2_LASER2D_FAKE_LASER_DRIVER_H
+#define ORCA2_LASER2D_FAKE_LASER_DRIVER_H
+
+#include <orcaice/context.h>
+#include <laser2dutil/driver.h>
+
+
+namespace laser2d {
+
+//
+// Bogus laser driver, useful for testing the rest of the component.
+//
+class FakeDriver : public Driver
+{
+
+public:
+
+    FakeDriver( const Config &cfg, const orcaice::Context &context );
+    virtual ~FakeDriver();
+
+    // Blocks till new data is available
+    virtual void read( float *ranges, unsigned char *intensities, orca::Time &timeStamp );
+
+private:
+
+    Driver::Config config_;
+    orcaice::Context context_;
+};
+
+// Used for dynamically loading driver
+class FakeDriverFactory : public laser2d::DriverFactory
+{
+public:
+    laser2d::Driver *createDriver( const Driver::Config &config,
+                                   const orcaice::Context &context ) const
+        {
+            return new FakeDriver( config, context );
+        }
+};
+
+} // namespace
+
+// Used for dynamically loading driver
+extern "C" {
+    laser2d::DriverFactory *createDriverFactory();
+}
+
+#endif
