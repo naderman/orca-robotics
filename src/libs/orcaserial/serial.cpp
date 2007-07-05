f69e83f5ea014f2df9db1f4fdc8924b029202a11
acfr <acfr@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-07-05 05:43:40 +0000

    *** empty log message ***
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@2666 534d5172-be3c-0410-9a3e-ccaa6372a113

--- src/libs/orcaserial/serial.cpp
+++ src/libs/orcaserial/serial.cpp
@@ -18,6 +18,11 @@
 #include <iostream>
 #include "serial.h"
 #include <sstream>
+#include <assert.h>
+
+#if __linux
+#  include <linux/serial.h>
+#endif
  
 #include <config.h>
 #if HAVE_FILIO_H
@@ -35,6 +40,83 @@
 using namespace std;
 
 namespace orcaserial {
+
+    namespace {
+
+        // Converts an integer baud-rate into a c-style '#define'd baudrate
+        int cBaudrate( int baudRate )
+        {
+            switch(baudRate)
+            {
+            case 0:
+                return B0;
+            case 50:
+                return B50;
+            case 75:
+                return B75;
+            case 110:
+                return B110;
+            case 134:
+                return B134;
+            case 150:
+                return B150;
+            case 200:
+                return B200;
+            case 300:
+                return B300;
+            case 600:
+                return B600;
+            case 1200:
+                return B1200;
+            case 1800:
+                return B1800;
+            case 2400:
+                return B2400;
+            case 4800:
+                return B4800;
+            case 9600:
+                return B9600;
+            case 19200:
+                return B19200;
+            case 38400:
+                return B38400;
+            case 57600:
+                return B57600;
+            case 115200:
+                return B115200;
+            case 230400:
+                return B230400;
+            case 460800:
+                return B460800;
+            case 500000:
+                return B500000;
+            case 576000:
+                return B576000;
+            case 921600:
+                return B921600;
+            case 1000000:
+                return B1000000;
+            case 1152000:
+                return B1152000;
+            case 1500000:
+                return B1500000;
+            case 2000000:
+                return B2000000;
+            case 2500000:
+                return B2500000;
+            case 3000000:
+                return B3000000;
+            case 3500000:
+                return B3500000;
+            case 4000000:
+                return B4000000;
+            default:
+                stringstream ss;
+                ss << "Serial::baud() Invalid baud rate: " << baudRate;
+                throw SerialException( ss.str() );
+            }            
+        }
+    }
     
 Serial::Serial( const char *dev,
                 int baudRate,
@@ -60,14 +142,16 @@ Serial::close()
     if ( debugLevel_ > 0 )
         cout<<"TRACE(serial.cpp): close()" << endl;
 
-    if(portFd_!=-1)
+    assert( portFd_ != -1 );
+
+    if(tcdrain(portFd_))
     {
-        if(tcdrain(portFd_))
-        {
-            perror("Serial::close():tcdrain()");
-        }
-        ::close(portFd_);
-    }    
+        perror("Serial::close():tcdrain()");
+    }
+    if (::close(portFd_))
+    {
+        perror("Serial::close():close()");
+    }
 }
 
 #ifdef __linux
@@ -89,111 +173,19 @@ Serial::setBaudRate(int baud)
         throw SerialException( ss.str() );
     }
 
-    int baudId;
-    switch(baud)
-    {
-    case 0:
-        baudId = B0;
-        break;
-    case 50:
-        baudId = B50;
-        break;
-    case 75:
-        baudId = B75;
-        break;
-    case 110:
-        baudId = B110;
-        break;
-    case 134:
-        baudId = B134;
-        break;
-    case 150:
-        baudId = B150;
-        break;
-    case 200:
-        baudId = B200;
-        break;
-    case 300:
-        baudId = B300;
-        break;
-    case 600:
-        baudId = B600;
-        break;
-    case 1200:
-        baudId = B1200;
-        break;
-    case 1800:
-        baudId = B1800;
-        break;
-    case 2400:
-        baudId = B2400;
-        break;
-    case 4800:
-        baudId = B4800;
-        break;
-    case 9600:
-        baudId = B9600;
-        break;
-    case 19200:
-        baudId = B19200;
-        break;
-    case 38400:
-        baudId = B38400;
-        break;
-    case 57600:
-        baudId = B57600;
-        break;
-    case 115200:
-        baudId = B115200;
-        break;
-    case 230400:
-        baudId = B230400;
-        break;
-    case 460800:
-        baudId = B460800;
-        break;
-    case 500000:
-        baudId = B500000;
-        break;
-    case 576000:
-        baudId = B576000;
-        break;
-    case 921600:
-        baudId = B921600;
-        break;
-    case 1000000:
-        baudId = B1000000;
-        break;
-    case 1152000:
-        baudId = B1152000;
-        break;
-    case 1500000:
-        baudId = B1500000;
-        break;
-    case 2000000:
-        baudId = B2000000;
-        break;
-    case 2500000:
-        baudId = B2500000;
-        break;
-    case 3000000:
-        baudId = B3000000;
-        break;
-    case 3500000:
-        baudId = B3500000;
-        break;
-    case 4000000:
-        baudId = B4000000;
-        break;
-    default:
-        stringstream ss;
-        ss << "Serial::baud() Invalid baud rate: " << baud;
-        throw SerialException( ss.str() );
-        break;
-    }
+    cfsetispeed(&serialOptions_, cBaudrate(baud));
+    cfsetospeed(&serialOptions_, cBaudrate(baud));
 
-    cfsetispeed(&serialOptions_, baudId);
-    cfsetospeed(&serialOptions_, baudId);
+    struct serial_struct  serinfo;
+    serinfo.reserved_char[0] = 0;
+    if (ioctl(portFd_, TIOCGSERIAL, &serinfo) < 0) 
+        throw SerialException( string("Serial::setBaudRate: ")+strerror(errno) );
+    
+    serinfo.flags &= ~ASYNC_SPD_CUST;
+    serinfo.custom_divisor = 0;
+    
+    if (ioctl(portFd_, TIOCSSERIAL, &serinfo) < 0) 
+        throw SerialException( string("Serial::setBaudRate: ")+strerror(errno) );
 
     if ( tcsetattr(portFd_, TCSAFLUSH, &serialOptions_) == -1 )
     {
@@ -466,6 +458,8 @@ Serial::write(const void *buf, size_t count)
     if ( debugLevel_ > 0 )
         cout<<"TRACE(serial.cpp): write()" << endl;
 
+    if ( count == 0 )
+        throw SerialException( "Serial::write() was called with zero bytes" );
 
     int put;
     put = ::write(portFd_, buf, count);
@@ -473,6 +467,15 @@ Serial::write(const void *buf, size_t count)
     {
         throw SerialException( string("Serial::write(): ")+strerror(errno) );
     }
+    else if ( put == 0 )
+    {
+        throw SerialException( "Serial::write(): ::write() returned 0" );
+    }
+    if ( debugLevel_ > 1 )
+    {
+        cout<<"TRACE(serial.cpp): wrote " << put << " bytes" << endl;
+    }
+
     return put;
 }
 
@@ -482,6 +485,9 @@ Serial::writeString(const char *str, size_t maxlen)
     if ( debugLevel_ > 0 )
         cout<<"TRACE(serial.cpp): writeString()" << endl;
 
+    if ( maxlen == 0 )
+        throw SerialException( "Serial::writeString() was called with zero bytes" );
+
     int toput=strnlen(str, maxlen);
     int put;
     put = ::write(portFd_, str, toput);
@@ -489,6 +495,15 @@ Serial::writeString(const char *str, size_t maxlen)
     {
         throw SerialException( string("Serial::write(): ")+strerror(errno) );
     }
+    else if ( put == 0 )
+    {
+        throw SerialException( "Serial::writeString(): ::write() returned 0" );
+    }
+    if ( debugLevel_ > 1 )
+    {
+        cout<<"TRACE(serial.cpp): wrote " << put << " bytes" << endl;
+    }
+
     return put;
 }
 
---
+++ src/libs/orcaserial/test/serialechotest.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <orcaserial/serial.h>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iomanip> 
+
+using namespace std;
+
+//
+// A simple utility program for testing serial ports.
+//
+
+std::string
+toHexString( const std::vector<unsigned char> &data )
+{
+    stringstream ss;
+    ss << "[ ";
+    for ( int i=0; i < data.size(); i++ )
+    {
+        ss <<hex<<std::setfill('0')<<std::setw(2)<<(int)(data[i])<<" ";
+    }
+    ss << "]";
+    return ss.str();    
+}
+
+int main( int argc, char **argv )
+{
+    if ( argc < 3 )
+    {
+        cout << "USAGE: " << argv[0] << " <dev> <baudrate>";
+    }
+
+    const bool isBlockingMode = true;
+    orcaserial::Serial serial( argv[1], atoi(argv[2]), isBlockingMode );
+    serial.setTimeout( 1, 0 );
+
+    std::vector<unsigned char> data;
+    while ( true )
+    {
+        cout<<"TRACE(serialechotest.cpp): Waiting for data..." << endl;
+        int nBytes = serial.bytesAvailableWait();
+
+        if ( nBytes > 0 )
+        {
+            cout<<"TRACE(serialechotest.cpp): nBytes: " << nBytes << endl;
+            if ( nBytes > data.size() )
+            {
+                cout<<"TRACE(serialechotest.cpp): resizing to " << nBytes << endl;
+                data.resize( nBytes );
+            }
+
+//             cout<<"TRACE(serialechotest.cpp): data.size(): " << data.size() << endl;
+//             cout<<"TRACE(serialechotest.cpp): &(data[0]): " << (int)(&(data[0])) << endl;
+//             cout<<"TRACE(serialechotest.cpp): data[0]: " << (int)(data[0]) << endl;
+
+            serial.read( &(data[0]), nBytes );
+
+            cout << "got data: " << toHexString( data ) << endl;
+        }
+    }
+
+    return 0;
+}
