9ee0b4e4a857c1f5f872bbde9b221c6eefeeaf3b
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-08-25 23:25:54 +0000

    added convenience functions for manipulation of poses (adding offsets etc).
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@2919 534d5172-be3c-0410-9a3e-ccaa6372a113

--- src/libs/orcanavutil/pose.h
+++ src/libs/orcanavutil/pose.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <assert.h>
+#include <orcanavutil/offset.h>
 
 namespace orcanavutil {
 
@@ -46,8 +47,49 @@ private:
     double x_, y_, theta_;
 
 };
-
 std::ostream &operator<<( std::ostream &s, const Pose &p );
+
+//
+// Pose manipulation convenience functions (see offset.h)
+//
+
+inline void addPoseOffset( Pose       &pose,
+                           const Pose &offset,
+                           bool        normaliseHeading )
+{
+    addPoseOffset( pose.x(), pose.y(), pose.theta(),
+                   offset.x(), offset.y(), offset.theta(),
+                   normaliseHeading );
+}
+
+inline void addPoseOffset( const Pose &start,
+                           const Pose &offset,
+                           Pose       &result,
+                           bool        normaliseHeading )
+{
+    addPoseOffset( start.x(), start.y(), start.theta(),
+                   offset.x(), offset.y(), offset.theta(),
+                   result.x(), result.y(), result.theta(),
+                   normaliseHeading );
+}
+
+inline void subtractInitialOffset( Pose       &pose,
+                                   const Pose &initialOffset )
+{
+    subtractInitialOffset( pose.x(), pose.y(), pose.theta(),
+                           initialOffset.x(), initialOffset.y(), initialOffset.theta() );
+}
+
+inline void subtractInitialOffset( const Pose &pose,
+                                   const Pose &initialOffset,
+                                   Pose       &result )
+{
+    subtractInitialOffset( pose.x(), pose.y(), pose.theta(),
+                           initialOffset.x(), initialOffset.y(), initialOffset.theta(),
+                           result.x(), result.y(), result.theta() );
+}
+
+
 
 }
 
