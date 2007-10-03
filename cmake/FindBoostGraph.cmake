53c923ae9f1d6b3647c629c0f4ec1e6b5db5699c
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-10-03 03:37:48 +0000

    new cmake macros to standardize messages when required software is not found.
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@3074 534d5172-be3c-0410-9a3e-ccaa6372a113

--- cmake/FindBoostGraph.cmake
+++ cmake/FindBoostGraph.cmake
@@ -3,7 +3,7 @@
 # This module defines
 # BOOST_GRAPH_FOUND, If false, don't try to use Boost Graph Library
 
-CHECK_INCLUDE_FILE_CXX( boost/numeric/graph/adjacency_list.hpp BOOST_GRAPH_FOUND )
+CHECK_INCLUDE_FILE_CXX( boost/graph/adjacency_list.hpp BOOST_GRAPH_FOUND )
 
 IF ( BOOST_GRAPH_FOUND )
     MESSAGE( STATUS "Looking for Boost Graph - found")
