92718472a18219635362cdd9a303f68aea331c5c
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-10-10 15:53:29 +0000

    towards more modular build macros.
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@3094 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ cmake/UseOrcaInterfaces.cmake
@@ -0,0 +1,8 @@
+#
+# For use in satellite projects
+#
+
+# Set up the include and library paths
+INCLUDE_DIRECTORIES( ${ORCA2_HOME}/include )
+LINK_DIRECTORIES( ${ORCA2_HOME}/lib )
+LINK_LIBRARIES( OrcaInterfaces )
