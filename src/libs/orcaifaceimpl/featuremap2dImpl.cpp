72568d616165bcd439a52d7cb266f2de91dc514a
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2007-11-08 22:10:32 +0000

    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@3470 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ src/libs/orcaifaceimpl/featuremap2dImpl.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <orcaice/orcaice.h>
+#include "util.h"
+#include "featuremap2dImpl.h"
+
+using namespace std;
+
+namespace orcaifaceimpl {
+
+//////////////////////////////////////////////////////////////////////
+
+//
+// This is the implementation of the slice-defined interface
+//
+class FeatureMap2dI : virtual public ::orca::FeatureMap2d
+{
+public:
+
+    FeatureMap2dI( FeatureMap2dImpl &impl )
+        : impl_(impl) {}
+
+    // remote functions
+
+    virtual ::orca::FeatureMap2dData getData(const Ice::Current&) const
+        { return impl_.internalGetData(); }
+
+    virtual void subscribe(const ::orca::FeatureMap2dConsumerPrx& consumer,
+                           const Ice::Current&)
+        { impl_.internalSubscribe( consumer ); }
+
+    virtual void unsubscribe(const ::orca::FeatureMap2dConsumerPrx& consumer,
+                 const Ice::Current&)
+        { impl_.internalUnsubscribe( consumer ); }
+
+private:
+    FeatureMap2dImpl &impl_;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+FeatureMap2dImpl::FeatureMap2dImpl( const std::string       &interfaceTag, 
+                                    const orcaice::Context  &context  )
+    : interfaceName_(getInterfaceNameFromTag(context,interfaceTag)),
+      topicName_(getTopicNameFromInterfaceName(context,interfaceName_)),
+      context_(context)
+{
+}
+
+FeatureMap2dImpl::FeatureMap2dImpl( const orcaice::Context  &context,
+                                    const std::string       &interfaceName )
+    : interfaceName_(interfaceName),
+      topicName_(getTopicNameFromInterfaceName(context,interfaceName)),
+      context_(context)
+{
+}
+
+FeatureMap2dImpl::~FeatureMap2dImpl()
+{
+    tryRemoveInterface( context_, interfaceName_ );
+}
+
+void
+FeatureMap2dImpl::initInterface()
+{
+    // Find IceStorm Topic to which we'll publish
+    topicPrx_ = orcaice::connectToTopicWithString<orca::FeatureMap2dConsumerPrx>
+        ( context_, consumerPrx_, topicName_ );
+
+    // Register with the adapter
+    // We don't have to clean up the memory we're allocating here, because
+    // we're holding it in a smart pointer which will clean up when it's done.
+    ptr_ = new FeatureMap2dI( *this );
+    orcaice::createInterfaceWithString( context_, ptr_, interfaceName_ );
+}
+
+void 
+FeatureMap2dImpl::initInterface( hydroutil::Thread* thread, int retryInterval )
+{
+    topicPrx_ = orcaice::connectToTopicWithString( context_, consumerPrx_, topicName_, thread, retryInterval );
+
+    ptr_ = new FeatureMap2dI( *this );
+    orcaice::createInterfaceWithString( context_, ptr_, interfaceName_, thread, retryInterval );
+}
+
+::orca::FeatureMap2dData
+FeatureMap2dImpl::internalGetData() const
+{
+    context_.tracer()->debug( "FeatureMap2dImpl::internalGetData()", 5 );
+
+    if ( dataProxy_.isEmpty() )
+    {
+        std::stringstream ss;
+        ss << "No data available! (interface="<<interfaceName_<<")";
+        throw orca::DataNotExistException( ss.str() );
+    }
+
+    orca::FeatureMap2dData data;
+    dataProxy_.get( data );
+    return data;
+}
+
+void
+FeatureMap2dImpl::internalSubscribe(const ::orca::FeatureMap2dConsumerPrx& subscriber)
+{   
+    context_.tracer()->debug( "FeatureMap2dImpl::internalSubscribe(): subscriber='"+subscriber->ice_toString()+"'", 4 );
+    try {
+        topicPrx_->subscribeAndGetPublisher( IceStorm::QoS(), subscriber->ice_twoway() );
+    }
+    catch ( const IceStorm::AlreadySubscribed & e ) {
+        std::stringstream ss;
+        ss <<"Request for subscribe but this proxy has already been subscribed, so I do nothing: "<< e;
+        context_.tracer()->debug( ss.str(), 2 );
+    }
+    catch ( const Ice::Exception & e ) {
+        std::stringstream ss;
+        ss <<"FeatureMap2dImpl::internalSubscribe: failed to subscribe: "<< e << endl;
+        context_.tracer()->warning( ss.str() );
+        throw orca::SubscriptionFailedException( ss.str() );
+    }
+}
+
+void
+FeatureMap2dImpl::internalUnsubscribe(const ::orca::FeatureMap2dConsumerPrx& subscriber)
+{
+    context_.tracer()->debug( "FeatureMap2dImpl::internalUnsubscribe(): subscriber='"+subscriber->ice_toString()+"'", 4 );
+    topicPrx_->unsubscribe( subscriber );
+}
+
+void
+FeatureMap2dImpl::localSetAndSend( const orca::FeatureMap2dData& data )
+{
+    dataProxy_.set( data );
+    
+    // Try to push to IceStorm.
+    tryPushToIceStormWithReconnect<orca::FeatureMap2dConsumerPrx,orca::FeatureMap2dData>
+        ( context_,
+          consumerPrx_,
+          data,
+          topicPrx_,
+          interfaceName_,
+          topicName_ );
+}
+
+}
+
---
+++ src/libs/orcaifaceimpl/featuremap2dImpl.h
@@ -0,0 +1,69 @@
+#ifndef ORCA_FEATUREMAP2D_IMPL_H
+#define ORCA_FEATUREMAP2D_IMPL_H
+
+// include defnition of Ice runtime
+#include <Ice/Ice.h>
+#include <IceStorm/IceStorm.h>
+
+// include provided interfaces
+#include <orca/featuremap2d.h>
+
+// utilities
+#include <hydroutil/proxy.h>
+#include <hydroutil/thread.h>
+#include <orcaice/context.h>
+
+namespace orcaifaceimpl
+{
+    
+//!
+//! Implements the FeatureMap2dIface interface: Handles remote calls.
+//!
+class FeatureMap2dImpl : public IceUtil::Shared
+{
+friend class FeatureMap2dI;
+
+public:
+    //! Constructor using interfaceTag (may throw ConfigFileException)
+    FeatureMap2dImpl( const std::string       &interfaceTag, 
+                      const orcaice::Context  &context );
+    //! constructor using interfaceName
+    FeatureMap2dImpl( const orcaice::Context  &context,
+                      const std::string       &interfaceName );
+    ~FeatureMap2dImpl();
+    
+    // local functions
+    //! may throw hydroutil::Exceptions
+    void initInterface();
+
+    //! Sets up interface and connects to IceStorm. Catches all exceptions and retries
+    //! until sucessful. At every iteration, checks if the thread was stopped.
+    void initInterface( hydroutil::Thread* thread, int retryInterval=2 );
+
+    //! A local call which sets the data reported by the interface, 
+    //! and sends it through IceStorm
+    void localSetAndSend( const orca::FeatureMap2dData &data );
+
+private:
+    // remote call implementations, mimic (but do not inherit) the orca interface
+    ::orca::FeatureMap2dData internalGetData() const;
+    void internalSubscribe(const ::orca::FeatureMap2dConsumerPrx&);
+    void internalUnsubscribe(const ::orca::FeatureMap2dConsumerPrx& );
+
+    hydroutil::Proxy<orca::FeatureMap2dData> dataProxy_;
+
+    orca::FeatureMap2dConsumerPrx    consumerPrx_;
+    IceStorm::TopicPrx       topicPrx_;
+
+    // Hang onto this so we can remove from the adapter and control when things get deleted
+    Ice::ObjectPtr          ptr_;
+
+    const std::string        interfaceName_;
+    const std::string        topicName_;
+    orcaice::Context         context_;
+};
+typedef IceUtil::Handle<FeatureMap2dImpl> FeatureMap2dImplPtr;
+
+}
+
+#endif
