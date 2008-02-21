2493a9ba1c633fba53f02890277441178906b9b1
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2008-02-21 23:58:01 +0000

    fixed errors in looking for gearbox
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@4147 534d5172-be3c-0410-9a3e-ccaa6372a113

--- cmake/global_setup.cmake
+++ cmake/global_setup.cmake
@@ -71,12 +71,12 @@ INCLUDE( ${PROJECT_SOURCE_DIR}/cmake/local/compiler.cmake )
 # Official dependency number 1: Gearbox
 # (this dependency is currently optional)
 #
-IF ( DEFINED GEARBOX_HOME )
+IF ( DEFINED GEARBOX_HOME AND GEARBOX_HOME )
     # the variable is specified with a command line option or is already in cache
     MESSAGE( STATUS "Gearbox location was specified or using cached value: ${GEARBOX_HOME}")
     # this is a hack, we'll require gearbox soon anyway
     SET( GEARBOX_FOUND 1 )
-ELSE ( DEFINED HYDRO_HOME )
+ELSE ( DEFINED GEARBOX_HOME AND GEARBOX_HOME )
     # not specified, need to find it
     INCLUDE ( ${HYDRO_CMAKE_DIR}/FindGearbox.cmake )
     ASSERT( GEARBOX_FOUND 
@@ -84,26 +84,26 @@ ELSE ( DEFINED HYDRO_HOME )
 #             "Looking for Gearbox - not found. Please install Gearbox, ** delete CMakeCache.txt **, then re-run CMake." 
             "Looking for Gearbox - found in ${GEARBOX_HOME}" 
             0 )
-ENDIF ( DEFINED GEARBOX_HOME )
+ENDIF ( DEFINED GEARBOX_HOME AND GEARBOX_HOME )
 
 #
 # Load Gearbox manifest
 #
 IF ( GEARBOX_FOUND )
-INCLUDE( ${GEARBOX_HOME}/gearbox_manifest.cmake )
-ASSERT( GEARBOX_MANIFEST_LOADED 
-        "Loading Gearbox manifest - failed." 
-#         "Loading Gearbox manifest - failed. Please reinstall Gearbox, ** delete CMakeCache.txt **, then re-run CMake." 
-        "Loading Gearbox manifest - loaded." 
-        0 )
-
-# Test Gearbox installation 
-# (we don't have any specific requirements yet)
-# INCLUDE ( ${ORCA_CMAKE_DIR}/TestGearbox.cmake )
-# ASSERT ( GEARBOX_WORKS
-#          "Testing Gearbox - failed. Please check or reinstall it, ** delete CMakeCache.txt **, then re-run CMake."
-#          "Testing Gearbox - ok."
-#          1 )
+    INCLUDE( ${GEARBOX_HOME}/gearbox_manifest.cmake )
+    ASSERT( GEARBOX_MANIFEST_LOADED 
+            "Loading Gearbox manifest - failed." 
+    #         "Loading Gearbox manifest - failed. Please reinstall Gearbox, ** delete CMakeCache.txt **, then re-run CMake." 
+            "Loading Gearbox manifest - loaded." 
+            0 )
+    
+    # Test Gearbox installation 
+    # (we don't have any specific requirements yet)
+    # INCLUDE ( ${ORCA_CMAKE_DIR}/TestGearbox.cmake )
+    # ASSERT ( GEARBOX_WORKS
+    #          "Testing Gearbox - failed. Please check or reinstall it, ** delete CMakeCache.txt **, then re-run CMake."
+    #          "Testing Gearbox - ok."
+    #          1 )
 ENDIF ( GEARBOX_FOUND )
 
 #
