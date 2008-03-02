1b5d1a2010391efe89b9f9e354936444062299e9
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2008-03-02 12:01:57 +0000

    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@4189 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ src/components/segwayrmp/estop.h
@@ -0,0 +1,52 @@
+#ifndef SEGWAYRMP_ESTOP_H
+#define SEGWAYRMP_ESTOP_H
+
+#include <IceUtil/Mutex.h>
+#include <orcaifaceimpl/estopImpl.h>
+#include <hydroiceutil/timer.h>
+
+namespace segwayrmp {
+
+//
+// @brief Establishes an orca::EStop interface, and works out when 
+//        the e-stop needs to be activated.
+//
+// Note: we begin in the EStop-triggered state: ie we're not moving unless
+//       someone explicitly says it's OK.
+//
+// @author Alex Brooks
+//
+class EStop : public orcaifaceimpl::EStopNonStandardImpl
+{
+
+public: 
+
+    EStop( double                  keepAlivePeriodSec,
+           const orcaice::Context &context );
+
+    // If true, we should _NOT_MOVE_ !!
+    bool isEStopTriggered();
+
+    // Inherited from orcaifaceimpl::EStopNonStandardImpl
+    void activateEStop();
+    void keepAlive();
+    double getRequiredKeepAlivePeriodSec();
+    void setToOperatingMode();
+
+private: 
+
+    IceUtil::Mutex mutex_;
+
+    bool                isEStopTriggered_;
+    double              keepAlivePeriodSec_;
+    hydroiceutil::Timer keepAliveTimer_;
+
+    orcaifaceimpl::EStopImplPtr eStopImpl_;
+
+    orcaice::Context context_;
+
+};
+
+}
+
+#endif
