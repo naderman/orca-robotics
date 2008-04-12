785004270ad9049a54c832f6b01f3518297a0d0b
tobasco <tobasco@534d5172-be3c-0410-9a3e-ccaa6372a113>
2008-04-12 00:05:30 +0000

    Configuration of elements changed: no consecutive numbers required any more
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@4386 534d5172-be3c-0410-9a3e-ccaa6372a113

--- src/libs/orcaqgui/configfileelements.cpp
+++ src/libs/orcaqgui/configfileelements.cpp
@@ -13,6 +13,7 @@
 #include <iostream>
 #include <orcaice/orcaice.h>
 #include <orcaobj/orcaobj.h>
+#include <hydroutil/stringutils.h> 
 
 using namespace std;
 
@@ -27,58 +28,72 @@ namespace orcaqgui {
 
         guiElementModel.createGuiElement( elementType, elementDetails );
     }
-
+    
+    
     void
     loadElementsFromConfigFile( orcaqgemv::GuiElementModel &guiElementModel,
-                                const orcaice::Context &context )
+                                const orcaice::Context     &context )
     {
-        // get properties from config file
-        string prefix = context.tag() + ".Config.Element";
-        int i=0;
-        QString elementType;
-        QStringList elementDetails;
-    
-        while (true)
+        // create a map of types
+        const string typePrefix = context.tag() + ".Config.Element.Type";
+        std::map<string,string> typeMap = context.properties()->getPropertiesForPrefix(typePrefix);
+        
+        // create a map of details
+        const string detailsPrefix = context.tag() + ".Config.Element.Detail";
+        std::map<string,string> detailMap = context.properties()->getPropertiesForPrefix(detailsPrefix);
+        
+        // debug output
+        for ( map<string,string>::iterator it=typeMap.begin(); it!=typeMap.end(); ++it )
         {
-            stringstream key;
-            key << prefix << i;
-            Ice::StringSeq strOut;
-        
-            //  Find elementType
-            int ret = orcaobj::getPropertyAsStringSeq( context.properties(), key.str()+".Type", strOut );
-            if (ret!=0) break;
-            elementType = QString(strOut[0].c_str());
-        
-            //  Find elementDetails
-            ret = orcaobj::getPropertyAsStringSeq( context.properties(), key.str()+".Detail", strOut );
-            elementDetails.clear();
-            if (ret!=0)
-            {
-                // no detail means special element with no interfaces like Grid
-                elementDetails.push_back("local@global/local");
-            }
-            else
-            {
-                for ( unsigned int k=0; k<strOut.size(); k++ )
-                {
-                    elementDetails.push_back(QString(strOut[k].c_str()));
-                }
-            }
-
-            // Debug output
-            cout << "TRACER(configfileelements.cpp): Loading element of type: " << elementType.toStdString() << endl;
-            cout << "TRACER(configfileelements.cpp): Loading element with details: ";
-            for (int k=0; k<elementDetails.size(); k++)
-            {
-                cout << elementDetails[k].toStdString() << " ";
-            }
-            cout << endl;
-            // end of debug output
-        
-            guiElementModel.createGuiElement( elementType, elementDetails );
-            i++;
+            stringstream ss;
+            ss << "it->first: " << it->first << endl;
+            ss << "it->second: " << it->second << endl;  
+            context.tracer().debug( ss.str(), 5 );
         }
-    }
+        
+        // create elements based on Type entries
+        for ( map<string,string>::iterator it = typeMap.begin(); it != typeMap.end(); ++it )
+        {
+            // extract the key: e.g. for Config.Element.TypeXX the key is XX
+            QString elementType( it->second.c_str() );
+            string key = it->first.substr( typePrefix.size() );
+            stringstream ss; ss << "Extracted key is: " << key;
+            context.tracer().debug( ss.str(), 5 );
+            
+            // find the corresponding entry in the detailMap
+            map<string,string>::iterator detailMapIt = detailMap.find( context.tag() + ".Config.Element.Detail" + key );
+            if ( detailMapIt==detailMap.end() ) 
+            {
+                ss.str(""); ss << "'Detail' entry with key " << key << " expected. Check your config file!";
+                context.tracer().warning( ss.str() );
+                continue;
+            }
+            
+            // found an entry
+            ss.str(""); ss << "Found Detail entry: " << detailMapIt->second;
+            context.tracer().debug( ss.str(), 5 );
+            
+            // some elements may have multiple interfaces: split details into separate strings
+            const char delim = ':';
+            Ice::StringSeq seq = hydroutil::toStringSeq( detailMapIt->second, delim );
+            
+            QStringList elementDetails;
+            for ( unsigned int k=0; k<seq.size(); k++ ) {
+                elementDetails.push_back(QString(seq[k].c_str()));
+            }
+            
+            // debug output
+            for ( int i=0; i<elementDetails.size(); i++ )
+            {
+                stringstream ss; ss << "Detail of interface " << i << ": " << elementDetails[i].toStdString();
+                context.tracer().debug( ss.str(), 5 );
+            }
+            
+            // finally: create the GuiElement
+            guiElementModel.createGuiElement( elementType, elementDetails );
+        }
 
+    }
+  
 }
 
