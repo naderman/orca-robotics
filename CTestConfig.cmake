173adba19225839dbb16e978e99c34f96aaa3370
tobasco <tobasco@534d5172-be3c-0410-9a3e-ccaa6372a113>
2008-04-17 06:20:53 +0000

    new dashboard
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@4412 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "Orca")
+set(CTEST_NIGHTLY_START_TIME "04:00:00 EST")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "cdash.acfr.usyd.edu.au")
+set(CTEST_DROP_LOCATION "/submit.php?project=Orca")
+set(CTEST_DROP_SITE_CDASH TRUE)
