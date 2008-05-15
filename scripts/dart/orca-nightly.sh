1fe53408d626b04ed378c32866dcab3f5c4fb6de
tobasco <tobasco@534d5172-be3c-0410-9a3e-ccaa6372a113>
2008-05-15 07:07:46 +0000

    fixed tests (hopefully)
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@4579 534d5172-be3c-0410-9a3e-ccaa6372a113

--- scripts/dart/orca-nightly.sh
+++ scripts/dart/orca-nightly.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+export TZ="UTC"
 dashboard=$HOME/ctests/orca/orca-nightly
+
+# required for prop tests
+export LD_LIBRARY_PATH=$HOME/ctests/orca/orca-nightly/build-orca/src/libs/orcaice/test:$LD_LIBRARY_PATH
 
 # compile with gcc-4.2
 logfile=$dashboard/orca.log
 echo ---------------------------------------- >> $logfile
 date >> $logfile
 echo ---------------------------------------- >> $logfile
+
+# make clean first
+#cd $dashboard/build-orca >> $logfile 2>&1
+#make clean
+
 # build and test
 /usr/bin/ctest -S $dashboard/orca-nightly-linux-gcc42.cmake -V >> $logfile 2>&1
+
 # after testing, install so orca-dependent nightly tests will work
 cd $dashboard/build-orca >> $logfile 2>&1
 make install >> $logfile 2>&1
