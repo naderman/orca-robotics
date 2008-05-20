86434cdab0b4d75a2581993b8a351c2c03faef1c
tobasco <tobasco@534d5172-be3c-0410-9a3e-ccaa6372a113>
2008-05-20 16:22:49 +0000

    updated cdash docco and scripts
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@4601 534d5172-be3c-0410-9a3e-ccaa6372a113

--- scripts/dart/orca-and-friends-cont.sh
+++ scripts/dart/orca-and-friends-cont.sh
@@ -3,12 +3,14 @@
 dashboard=/home/users/dart/ctests/orca-and-friends/orca-and-friends-cont
 logfile=$dashboard/orca-and-friends.log
 
-#export HYDRO_INSTALL=/opt/hydro-cont
-#export HYDRO_HOME=$HYDRO_INSTALL
-#export LD_LIBRARY_PATH=$HYDRO_HOME/lib:$LD_LIBRARY_PATH
+export ICE_HOME=/opt/Ice
+export PATH=$ICE_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$ICE_HOME/lib:$LD_LIBRARY_PATH
+
+export TZ="UTC"
 
 while true; do
- 
+
  echo ------- ORCA AND FRIENDS ---------- >> $logfile
  date >> $logfile
  echo ----------------------------------- >> $logfile
@@ -20,21 +22,17 @@ while true; do
  cd $dashboard/build-gearbox >> $logfile 2>&1
  make install >> $logfile 2>&1
  cd ~ >> $logfile 2>&1
-   
+
  echo ===== Building Hydro  ===== >> $logfile
  /usr/bin/ctest -S $dashboard/hydro-cont-linux-gcc42.cmake -V >> $logfile 2>&1
- 
+
  echo ==== Installing Hydro ===== >> $logfile
  cd $dashboard/build-hydro >> $logfile 2>&1
  make install >> $logfile 2>&1
  cd ~ >> $logfile 2>&1
-    
+
  echo ===== Building and Testing Orca  ===== >> $logfile
  /usr/bin/ctest -S $dashboard/orca-cont-linux-gcc42.cmake -V >> $logfile 2>&1
- 
-# echo "===== Re-Building orca, to be 100% sure it is all built ===============" >> $logfile
-# cd $dashboard/build-orca >> $logfile 2>&1
-# make >> $logfile 2>&1
 
  echo ===== Installing Orca =============== >> $logfile
  cd $dashboard/build-orca >> $logfile 2>&1
@@ -43,12 +41,12 @@ while true; do
 
  echo ===== Building and Testing empty-project  ===== >> $logfile
  /usr/bin/ctest -S $dashboard/empty-cont-linux-gcc42.cmake -V >> $logfile 2>&1
-  
+
  echo ===== Building and Testing shh  ===== >> $logfile
  /usr/bin/ctest -S $dashboard/shh-cont-linux-gcc42.cmake -V >> $logfile 2>&1
-   
+
  echo ===== Finished  =============== >> $logfile
-  
+
 sleep 311
 
 done
