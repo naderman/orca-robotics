7857eeca65679a0548e4f40a56157327aefceb13
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2008-07-06 03:49:57 +0000

    invalidated non-maintained scripts.
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@4731 534d5172-be3c-0410-9a3e-ccaa6372a113

--- scripts/matlab/logutils/parseodometry2ddata.m
+++ scripts/matlab/logutils/parseodometry2ddata.m
@@ -4,6 +4,8 @@ function [time, position2d] = parseodometry2ddata( file )
 %    time = each row is a Unix timestamp
 %    odometry2d = each row is a data record for each timestamp: posX, posY, orientation, velX, velY, velOrientation
 
+error('AlexB: The Odometry ascii log file format has changed, and I have not updated this function: needs to be updated.');
+
 [sec, usec, posX, posY, orientation, velX, velY, velOrientation]...
      =textread(file, '%f %f \n %f %f %f %f %f %f', 'headerlines',1);
 
