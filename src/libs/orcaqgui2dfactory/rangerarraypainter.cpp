ccf30be5e8dfaccb8e414ae714756ec876001b40
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2009-03-28 01:57:54 +0000

    added files from RangerArray patch
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@5576 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ src/libs/orcaqgui2dfactory/rangerarraypainter.cpp
@@ -0,0 +1,185 @@
+/*
+ * Orca-Robotics Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2008 Alex Brooks, Alexei Makarenko, Tobias Kaupp
+ *
+ * This copy of Orca is licensed to you under the terms described in
+ * the LICENSE file included in this distribution.
+ *
+ */
+
+#include <iostream>
+#include <cmath>
+#include <assert.h>
+#include <QPainter>
+#include <QString>
+#include <QPolygon>
+
+#include <orcaice/orcaice.h>
+#include <orcaobj/orcaobj.h>
+
+#include "rangerarraypainter.h"
+
+using namespace std;
+using namespace orcaqgui2d;
+
+RangerArrayPainter::RangerArrayPainter( QColor outlineColor,
+                            float  outlineThickness,
+                            float  brightReturnWidth )
+    : isDisplayScan_(true),
+      isDisplayPoints_(false),
+      isFilledPolygon_(false),
+      outlineColor_(outlineColor),
+      basisColor_(outlineColor),
+      outlineThickness_(outlineThickness)
+{
+    fillColor_ = QColor(204,204,255,127);
+    outlineColor_ = basisColor_ = Qt::blue;
+}
+
+void
+RangerArrayPainter::clear()
+{
+    for ( unsigned int i=0; i<rangers_.size(); ++i )
+    {
+        rangers_[i].qScan.clear();
+        rangers_[i].qReturn.clear();
+        rangers_[i].qReturnArc.clear();
+    }
+}
+
+void
+RangerArrayPainter::setDescription( orca::RangerArrayDescription &description )
+{
+    description_ = description;
+}
+
+void
+RangerArrayPainter::setData( const orca::RangerArrayDataPtr & data )
+{
+    if ( data==0 ) return;
+
+    ranges_        = data->ranges;
+
+    if ( rangers_.size() != ranges_.size() )
+    {
+        rangers_.resize( ranges_.size() );
+    }
+    clear();
+
+    QPointF point;
+
+    for ( unsigned int i=0; i<rangers_.size(); ++i )
+    {
+	rangers_[i].qScan.push_back( QPointF(0.0,0.0) );    // first point in array is origin
+
+        // convert from polar to cartesian coord. sys.
+        const int DRAWN_POINT_COUNT = 5;
+        for ( int j = -DRAWN_POINT_COUNT / 2; j<=DRAWN_POINT_COUNT/2; ++j ) {
+
+            // account for sensor heading:
+	    // - only the x-coordinate becomes "compressed" by pitch
+	    // - only the y-coordinate becomes "compressed" by roll
+            double bearing = 0.5 * description_.detectionConeAngle * (j / (DRAWN_POINT_COUNT / 2));
+	    point.setX( ranges_[i] * cos( bearing ) * cos( description_.poses[i].o.p ) );
+    	    point.setY( ranges_[i] * sin( bearing ) * cos( description_.poses[i].o.r ) );
+
+	    rangers_[i].qScan.push_back( point );
+    	    rangers_[i].qReturnArc.push_back( point );
+	    if(j == 0)
+	    {
+        	rangers_[i].qReturn.push_back( point );
+    	    }
+	}
+    }
+}
+
+void
+RangerArrayPainter::paint( QPainter *painter, int z )
+{
+    if ( z != hydroqguielementutil::Z_RANGERARRAY || !isDisplayScan_ ) return;
+    if ( rangers_.empty() ) return;
+    
+    // scaling
+    qreal onePix = painter->worldMatrix().inverted().mapRect(QRectF(0, 0, 1, 1)).width();
+
+    for ( std::size_t rayNum=0; rayNum<rangers_.size(); ++rayNum )
+    {
+	painter->save();
+    
+	// handle offset of this ray
+	painter->translate( description_.poses[rayNum].p.x, description_.poses[rayNum].p.y );
+	painter->rotate( description_.poses[rayNum].o.y*180.0/M_PI );
+
+	if ( outlineThickness_ == -1 ) {
+    	    painter->setPen( QPen( outlineColor_ ) );
+	}
+	else {
+    	    painter->setPen( QPen( outlineColor_, outlineThickness_ ) );
+	}
+	if ( isFilledPolygon_ ) {
+    	    painter->setBrush( fillColor_ );
+	}
+	
+	//
+	// draw the scan itself
+	//
+	painter->drawConvexPolygon( rangers_[rayNum].qScan );
+
+	//
+	// draw the arc
+	//
+    	painter->setPen( QPen( outlineColor_, 3.0*onePix ) );
+    	painter->drawPolyline( rangers_[rayNum].qReturnArc );
+
+	if ( isDisplayPoints_ )
+	{
+    	    painter->setPen( QPen( Qt::black, 3.0*onePix ) );
+//         	painter->setPen( QPen( Qt::black ) );
+    	    painter->drawPoints( rangers_[rayNum].qReturn );
+	}
+
+	painter->restore();
+    }
+}
+
+void
+RangerArrayPainter::execute( int action )
+{
+    // debug
+    cout<<"RangerArrayPainter::execute: action #"<<action<<endl;
+
+    switch ( action )
+    {
+    case 0 :
+        // toggle scan
+        isDisplayScan_ = !isDisplayScan_;
+        break;
+    case 1 :
+        // toggle points
+        isDisplayPoints_ = !isDisplayPoints_;
+        cout<<"TRACE(rangerarraypainter.cpp): isDisplayPoints_ set to " << isDisplayPoints_ << endl;
+        break;
+    case 2 :
+        // toggle filling
+        isFilledPolygon_ = !isFilledPolygon_;
+        break;
+    case 3 :
+        // toggle transparency
+        if (fillColor_.alpha()==127) {
+            fillColor_.setAlpha(255);
+        } else {
+            fillColor_.setAlpha(127);
+        }
+        break;
+    }
+}
+
+void RangerArrayPainter::setFocus( bool inFocus )
+{
+    if (!inFocus) {
+        outlineColor_=Qt::gray;
+    } else {
+        outlineColor_=basisColor_;
+    }
+}
---
+++ src/libs/orcaqgui2dfactory/rangerarraypainter.h
@@ -0,0 +1,81 @@
+/*
+ * Orca-Robotics Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2008 Alex Brooks, Alexei Makarenko, Tobias Kaupp
+ *
+ * This copy of Orca is licensed to you under the terms described in
+ * the LICENSE file included in this distribution.
+ *
+ */
+
+#ifndef ORCAGUI_RANGERARRAY_PAINTER_H
+#define ORCAGUI_RANGERARRAY_PAINTER_H
+
+#include <QColor>
+#include <QPolygonF>
+#include <orca/rangerarray.h>
+#include <hydroqguielementutil/definitions2d.h>
+#include <hydroqgui/hydroqgui.h>
+
+class QPainter;
+
+namespace orcaqgui2d
+{
+
+/*!
+  @brief Paints ranger returns.
+*/
+class RangerArrayPainter
+{
+  
+  public:
+    // -1 means 'default'
+    RangerArrayPainter( QColor outlineColor=QColor( 102,153,102, 255 ),
+                            float  outlineThickness=-1,
+                            float  brightReturnWidth=0.2 );
+
+    void setDescription( orca::RangerArrayDescription &description );
+    void setData( const orca::RangerArrayDataPtr &scan );
+    
+    void paint( QPainter *p, int z );
+    bool paintThisLayer(int z) const {return z==hydroqguielementutil::Z_RANGERARRAY;}
+
+    void clear();
+    
+    void execute( int action );
+    
+    void setFocus( bool inFocus );
+
+  private:
+
+    std::vector<float>         ranges_;
+
+    // Data for every ray
+    struct RANGERDATA
+    {
+        // Scan (set of points for drawing the scan outline)
+        QPolygonF qScan;
+
+        // The actual return (doesn't include a point at the origin)
+	QPolygonF qReturn;
+
+	// The return arc (to make it more visible)
+	QPolygonF qReturnArc;
+    };
+    std::vector<RANGERDATA> rangers_;
+
+    bool isDisplayScan_;
+    bool isDisplayPoints_;
+    bool isFilledPolygon_;
+    QColor outlineColor_;
+    QColor fillColor_;
+    QColor basisColor_;
+    float  outlineThickness_;
+
+    orca::RangerArrayDescription description_;
+
+};
+
+} // namespace
+
+#endif
