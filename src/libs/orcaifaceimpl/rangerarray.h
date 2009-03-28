ec11df6a3b1a7bd38e3a8f13e5528ce488975966
borax00 <borax00@534d5172-be3c-0410-9a3e-ccaa6372a113>
2009-03-28 01:51:06 +0000

    added file from RangerArray patch
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@5574 534d5172-be3c-0410-9a3e-ccaa6372a113

---
+++ src/libs/orcaifaceimpl/rangerarray.h
@@ -0,0 +1,52 @@
+/*
+ * Orca-Robotics Project: Components for robotics 
+ *               http://orca-robotics.sf.net/
+ * Copyright (c) 2004-2008 Alex Brooks, Alexei Makarenko, Tobias Kaupp
+ *
+ * This copy of Orca is licensed to you under the terms described in
+ * the LICENSE file included in this distribution.
+ *
+ */
+
+#ifndef ORCAIFACEIMPL_RANGERARRAY_H
+#define ORCAIFACEIMPL_RANGERARRAY_H
+
+#include <orcaifaceimpl/providerWithDescriptionImpl.h>
+#include <orcaifaceimpl/storingconsumerImpl.h>
+#include <orcaifaceimpl/bufferedconsumerImpl.h>
+#include <orcaifaceimpl/notifyingconsumerImpl.h>
+#include <orcaifaceimpl/printingconsumerImpl.h>
+
+#include <orca/rangerarray.h>
+
+namespace orcaifaceimpl {
+
+typedef ProviderWithDescriptionImpl<orca::RangerArray, orca::RangerArrayPrx,
+                    orca::RangerArrayConsumer, orca::RangerArrayConsumerPrx,
+                    orca::RangerArrayDataPtr, orca::RangerArrayDescription> RangerArrayImpl;
+typedef IceUtil::Handle<RangerArrayImpl> RangerArrayImplPtr;
+
+typedef StoringConsumerImpl<orca::RangerArray, orca::RangerArrayPrx,
+                    orca::RangerArrayConsumer, orca::RangerArrayConsumerPrx,
+                    orca::RangerArrayDataPtr> StoringRangerArrayConsumerImpl;
+typedef IceUtil::Handle<StoringRangerArrayConsumerImpl> StoringRangerArrayConsumerImplPtr;
+
+typedef BufferedConsumerImpl<orca::RangerArray, orca::RangerArrayPrx,
+                        orca::RangerArrayConsumer, orca::RangerArrayConsumerPrx,
+                        orca::RangerArrayDataPtr> BufferedRangerArrayConsumerImpl;
+typedef IceUtil::Handle<BufferedRangerArrayConsumerImpl> BufferedRangerArrayConsumerImplPtr;
+
+typedef NotifyingConsumerImpl<orca::RangerArray, orca::RangerArrayPrx,
+                        orca::RangerArrayConsumer,
+                        orca::RangerArrayConsumerPrx,
+                        orca::RangerArrayDataPtr> NotifyingRangerArrayConsumerImpl;
+typedef IceUtil::Handle<NotifyingRangerArrayConsumerImpl> NotifyingRangerArrayConsumerImplPtr;
+
+typedef PrintingConsumerImpl<orca::RangerArray, orca::RangerArrayPrx,
+                        orca::RangerArrayConsumer, orca::RangerArrayConsumerPrx,
+                        orca::RangerArrayDataPtr> PrintingRangerArrayConsumerImpl;
+typedef IceUtil::Handle<PrintingRangerArrayConsumerImpl> PrintingRangerArrayConsumerImplPtr;
+
+}
+
+#endif
