c4901d3d67b30d2aa33586729db03f84981d463a
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2009-05-06 21:02:25 +0000

    define install dir explicitely
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@5607 534d5172-be3c-0410-9a3e-ccaa6372a113

--- cmake/internal/orca-config.cmake
+++ cmake/internal/orca-config.cmake
@@ -5,41 +5,48 @@
 set( ORCA_FOUND 1 )
 
 get_filename_component( _found_dir "${CMAKE_CURRENT_LIST_FILE}" PATH )
+
+# load all exported Orca targets
 include( ${_found_dir}/orca-targets.cmake )
+
+# assume that orca-config.cmake was installed into
+# <install-root>/lib/orca/ 
+set( _install_dir "${_found_dir}/../../" )
 
 get_filename_component( 
     ORCA_INCLUDE_DIR 
-    "${_found_dir}/../../include/orca" 
+    "${_install_dir}/include/orca" 
     ABSOLUTE )
 
 get_filename_component( 
     ORCA_CMAKE_DIR 
-    "${_found_dir}/../../share/orca/cmake"
+    "${_install_dir}/share/orca/cmake"
     ABSOLUTE )
 
 get_filename_component( 
     ORCA_USE_FILE 
-    "${_found_dir}/../../share/orca/cmake/orca-use-file.cmake"
+    "${_install_dir}/share/orca/cmake/orca-use-file.cmake"
     ABSOLUTE )
 
 get_filename_component( 
     ORCA_SLICE_DIR 
-    "${_found_dir}/../../share/orca/slice"
+    "${_install_dir}/share/orca/slice"
     ABSOLUTE )
 
 get_filename_component( 
     ORCA_LINK_DIR 
-    "${_found_dir}/../../lib/orca" 
+    "${_install_dir}/lib/orca" 
     ABSOLUTE )
 
 get_filename_component(
     ORCA_DEFTOOLS_BIN_DIR
-    "${_found_dir}/../../bin"
+    "${_install_dir}/bin"
     ABSOLUTE )
 
 get_filename_component(
     ORCA_SLICE2ORCA_BIN_DIR
-    "${_found_dir}/../../bin"
+    "${_install_dir}/bin"
     ABSOLUTE )
 
 set( _found_dir )
+set( _install_dir )
