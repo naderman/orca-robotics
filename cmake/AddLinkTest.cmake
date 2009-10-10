8bd8a85763e057bbe5ef93843d54583fea024341
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2009-10-10 01:48:16 +0000

    renamed *.template files used in linktest macro to more standard *.in
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@5661 534d5172-be3c-0410-9a3e-ccaa6372a113

--- cmake/AddLinkTest.cmake
+++ cmake/AddLinkTest.cmake
@@ -1,8 +1,8 @@
 #
 # HYDRO_ADD_LINK_TEST ( interface_name driver_namespace driver_header LIB0 [LIB1 LIB2 ...] )
 #
-# Expects to find 
-# - file component.h/cpp 
+# Expects to find
+# - file component.h/cpp
 # - which defines class ${COMP_NAMESPACE}::Component
 # - which derives from orcaice::Component
 #
@@ -10,14 +10,14 @@ macro( HYDRO_ADD_LINK_TEST interface_name driver_namespace driver_header )
 
     set( MAIN_CPP_FILE ${CMAKE_CURRENT_BINARY_DIR}/autogen/linktest.cpp )
 
-    set( interface_name ${interface_name} ) 
-    set( factory_name ${interface_name}Factory ) 
-    set( driver_namespace ${driver_namespace} ) 
+    set( interface_name ${interface_name} )
+    set( factory_name ${interface_name}Factory )
+    set( driver_namespace ${driver_namespace} )
     set( driver_header ${driver_header} )
 
     set( target_name ${driver_namespace}_linktest )
 
-    configure_file( ${ORCA_CMAKE_DIR}/linktest.cpp.template ${MAIN_CPP_FILE} )
+    configure_file( ${ORCA_CMAKE_DIR}/linktest.cpp.in ${MAIN_CPP_FILE} )
 
     add_executable( ${target_name} ${MAIN_CPP_FILE} )
     target_link_libraries( ${target_name} ${ARGN} )
@@ -31,14 +31,14 @@ macro( HYDRO_ADD_LINK_TEST_WITH_CONFIG interface_name driver_namespace driver_he
 
     set( MAIN_CPP_FILE ${CMAKE_CURRENT_BINARY_DIR}/autogen/linktest.cpp )
 
-    set( interface_name ${interface_name} ) 
-    set( factory_name ${interface_name}Factory ) 
-    set( driver_namespace ${driver_namespace} ) 
+    set( interface_name ${interface_name} )
+    set( factory_name ${interface_name}Factory )
+    set( driver_namespace ${driver_namespace} )
     set( driver_header ${driver_header} )
 
     set( target_name ${driver_namespace}_linktest )
 
-    configure_file( ${ORCA_CMAKE_DIR}/linktestwithconfig.cpp.template ${MAIN_CPP_FILE} )
+    configure_file( ${ORCA_CMAKE_DIR}/linktestwithconfig.cpp.in ${MAIN_CPP_FILE} )
 
     add_executable( ${target_name} ${MAIN_CPP_FILE} )
     target_link_libraries( ${target_name} ${ARGN} )
