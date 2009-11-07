445c5271c2ce1ec682f9daebac554dc4eab0cde5
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2009-11-07 03:40:43 +0000

    works with updated package-config macros
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@5665 534d5172-be3c-0410-9a3e-ccaa6372a113

--- cmake/internal/orca-config-internal.cmake
+++ cmake/internal/orca-config-internal.cmake
@@ -1,14 +1,18 @@
-set( ORCA_FOUND 1 )   
+# Find resources contained within Orca.
+# To be used by CMake projects co-located with Orca within one "super-project".
+#
+
+set( ORCA_FOUND 1 )
 
 # This is potentially problematic: installed directory structure is different from in-source one.
-set( ORCA_INCLUDE_DIR 
+set( ORCA_INCLUDE_DIR
     ${CMAKE_SOURCE_DIR}/orca/src/hydrolibs
     ${CMAKE_SOURCE_DIR}/orca/src           # for hydrointerfaces
     ${CMAKE_SOURCE_DIR}/orca/src/hydrodrivers
     ${CMAKE_SOURCE_DIR}/orca/src/libs
     ${CMAKE_BINARY_DIR}/orca/src/interfaces/cpp )
 
-message( STATUS "DEBUG!!! ORCA_INCLUDE_DIR=${ORCA_INCLUDE_DIR}" )
+# message( STATUS "DEBUG!!! ORCA_INCLUDE_DIR=${ORCA_INCLUDE_DIR}" )
 
 set( ORCA_CMAKE_DIR ${CMAKE_SOURCE_DIR}/orca/cmake )
 
@@ -16,14 +20,14 @@ set( ORCA_USE_FILE ${ORCA_CMAKE_DIR}/orca-use-file.cmake )
 
 set( ORCA_SLICE_DIR ${CMAKE_SOURCE_DIR}/orca/src/interfaces/slice )
 
-# this is where Orca libs will be installed 
+# this is where Orca libs will be installed
 set( ORCA_LINK_DIR ${CMAKE_INSTALL_PREFIX}/lib/orca )
 
 set( ORCA_DEFTOOLS_BIN_DIR ${CMAKE_BINARY_DIR}/orca/src/utils/deftools )
 set( ORCA_SLICE2ORCA_BIN_DIR ${CMAKE_BINARY_DIR}/orca/src/utils/slice2orca )
 
 # for compatibility with code which uses Hydro only
-set( HYDRO_INCLUDE_DIR 
+set( HYDRO_INCLUDE_DIR
     ${CMAKE_SOURCE_DIR}/orca/src/hydrolibs
     ${CMAKE_SOURCE_DIR}/orca/src           # for hydrointerfaces
     ${CMAKE_SOURCE_DIR}/orca/src/hydrodrivers )
