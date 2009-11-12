ca3e4c3f39f7ea642d1fe4eab263f3c3eda69e73
russo2503v <russo2503v@534d5172-be3c-0410-9a3e-ccaa6372a113>
2009-11-12 04:22:51 +0000

    added build/not build option to enable global switch off with GBX_DISABLE_ALL var
    git-svn-id: https://orca-robotics.svn.sf.net/svnroot/orca-robotics/orca/trunk@5681 534d5172-be3c-0410-9a3e-ccaa6372a113

--- cmake/BuildLibProjectInterfacesCpp.cmake
+++ cmake/BuildLibProjectInterfacesCpp.cmake
@@ -4,34 +4,34 @@ set( _lib_version ${GBX_PROJECT_VERSION} )
 GBX_ADD_LICENSE( LGPL )
 
 set( build TRUE )
-# GBX_REQUIRE_OPTION( build LIB ${_lib_name} ON )
+GBX_REQUIRE_OPTION( build LIB ${_lib_name} ON )
 
 if( build )
     #
     # Defines macros for parsing Slice source files.
     #
     include( ${ORCA_CMAKE_DIR}/Slice2Cpp.cmake )
-    
+
     #
     # All Slice source files are defined in one place (loads ORCA_SLICE_SOURCE_FILES)
     #
     include( ${PROJECT_SOURCE_DIR}/src/interfaces/slice/${GBX_PROJECT_NAME_LOWER}/slice_sources.cmake )
-    
+
     #
     # Work out the list of generated files from the list of slice sources
     #
     ORCA_GENERATE_SLICE2CPP_RULES( _gen_sources _gen_headers ${ORCA_SLICE_SOURCE_FILES} )
-    
+
     include( ${ORCA_CMAKE_DIR}/UseBasicRules.cmake )
     include( ${ORCA_CMAKE_DIR}/UseIce.cmake )
     include( ${ORCA_CMAKE_DIR}/UseLibProjectInterfaces.cmake )
- 
+
     include_directories( ${PROJECT_BINARY_DIR}/src/interfaces/cpp )
 
     if( NOT ORCA_MOTHERSHIP )
         include( ${ORCA_CMAKE_DIR}/UseOrca.cmake )
     endif( NOT ORCA_MOTHERSHIP )
-    
+
     # IceStorm is not included in UseIce.cmake
     set( _dep_libs IceStorm )
 
@@ -42,13 +42,13 @@ if( build )
     if( NOT ORCA_MOTHERSHIP AND IS_SUPER_PROJECT )
         add_dependencies( ${_lib_name} OrcaInterfaces )
     endif( NOT ORCA_MOTHERSHIP AND IS_SUPER_PROJECT )
-    
-    if( ORCA_MOTHERSHIP )    
+
+    if( ORCA_MOTHERSHIP )
         # only Orca installs generated headers
         foreach( _gen_header ${_gen_headers} )
             set( slice_generated_headers_full ${slice_generated_headers_full} ${CMAKE_CURRENT_BINARY_DIR}/${_gen_header} )
         endforeach( _gen_header )
-        
+
         GBX_ADD_HEADERS( ${_lib_namespace} ${slice_generated_headers_full} )
     endif( ORCA_MOTHERSHIP )
 
